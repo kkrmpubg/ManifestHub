38c0bb99ab736eaec367adb712308d612ba1c2c6
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-07-07 21:43:38 +0800

    Add files via upload

--- 3527290.lua
+++ 3527290.lua
@@ -1,13 +1,3 @@
--- Generated Lua Manifest by Solus Bot in Morrenus Games
--- Steam App 3527290 Manifest
--- Name: PEAK
--- Generated: 2025-07-03 08:33:13
--- Total Depots: 1
--- Total DLCs: 0
-
--- MAIN APPLICATION
-addappid(3527290) -- PEAK
-
--- MAIN APP DEPOTS
-addappid(3527291, 1, "f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05") -- Depot 3527291
-setManifestid(3527291, "2979913009316948998", 0)
+addappid(3527291,0,"f55585bb6a00ba332421256f80ba05e27afc38fec6170b9a763138244c506f05")
+setManifestid(3527291,"2979913009316948998")
+addappid(3527290)
