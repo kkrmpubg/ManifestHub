e6ae7aeecae7bde92094972f9204b4fc07bc0f13
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-08-19 18:04:24 +0800

    Add files via upload

---
+++ 2133760.lua
@@ -0,0 +1,11 @@
+addappid(2133760)
+addappid(2133762, 1, "d52e06a500706a15e7821f6a7c3e30d1a2bce0ea65b784d6a387cdadef756a78")
+addappid(2133763, 1, "788e7476ac625a7a50f7add2445dc120f1524ee6dc56b8f14edec6a2f954474a") 
+addappid(2133764, 1, "1ebff4822d7fe419fe87324ea8a05d3dc5b985322407afcd0ad15c10f340eba7") 
+addappid(3933690)
+
+
+--[[
+This file belongs to Ahmeds Bot Discord Server
+-----> join discord Server : https://discord.gg/sv6EGxCRnC
+]]
