890ecb40de59edda536ec02eb9a0998d2469ed4e
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-08-28 14:32:42 +0800

    Add files via upload

---
+++ 2001120.lua
@@ -0,0 +1,23 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 2001120 Manifest
+-- Name: Split Fiction
+-- Generated: 2025-06-07 07:26:51
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2001120) -- Split Fiction
+
+-- MAIN APP DEPOTS
+addappid(2001121, 1, "27f6aac2ce9d80ad4ed8fe3710d306ae108f00ddcf005d47ecf7bfa2a258964c") -- Main Game Content (Windows Content)
+setManifestid(2001121, "4899075162680269256", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "550968249685141759", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3050960) -- Split Fiction - Full Game Unlock
