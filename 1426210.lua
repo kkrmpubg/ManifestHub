f6590905e912418ad55abc6ca0263d43c335451d
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-09-10 18:16:57 +0800

    Add files via upload

---
+++ 1426210.lua
@@ -0,0 +1,23 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1426210 Manifest
+-- Name: It Takes Two
+-- Generated: 2025-06-06 03:17:46
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1426210) -- It Takes Two
+
+-- MAIN APP DEPOTS
+addappid(1426211, 1, "c450c44e032e27d0308cf864259fb2af91bcfc58401a0caf33bb8efba85bb323") -- It Takes Two - Content
+setManifestid(1426211, "5290425664756095500", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1426390) -- It Takes Two  Full Game Unlock
