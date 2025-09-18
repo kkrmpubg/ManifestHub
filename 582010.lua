a3ccbe1225ac5b39a08134817dd915fa97a915a4
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-09-18 22:23:07 +0800

    Add files via upload

---
+++ 582010.lua
@@ -0,0 +1,98 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 582010 Manifest
+-- Name: Monster Hunter: World
+-- Generated: 2025-06-06 11:21:13
+-- Total Depots: 2
+-- Total DLCs: 64
+
+-- MAIN APPLICATION
+addappid(582010) -- Monster Hunter: World
+
+-- MAIN APP DEPOTS
+addappid(582011, 1, "da6b97dc25e43b8f7ceaf9bcdbfba960f2e3e855b855fc47975d58a68d3f7688") -- MONSTER HUNTER: WORLD Content
+setManifestid(582011, "2525674006025364114", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- Monster Hunter World - High Resolution Texture Pack (AppID: 960781)
+addappid(960781)
+addappid(582012, 1, "92d06583c95939873c5c33658cd8b1111fa209a4c7576207565e942fa78eb888") -- Monster Hunter World - High Resolution Texture Pack - MONSTER HUNTER: WORLD Depot
+setManifestid(582012, "5526886592091189537", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(601100) -- Monster Hunter World - Origin Armor Set
+addtoken(601100, "17962436155033060771")
+addappid(743980) -- Monster Hunter World - Fair Wind Charm
+addtoken(743980, "1782879713785509081")
+addappid(759450) -- Monster Hunter World - Samurai Set
+addtoken(759450, "3563002530143689390")
+addappid(759451) -- Monster Hunter World - Gesture Zen
+addtoken(759451, "6574382732742458533")
+addappid(759452) -- Monster Hunter World - Gesture Ninja Star
+addtoken(759452, "3834830759551103192")
+addappid(759453) -- Monster Hunter World - Gesture Sumo Slap
+addtoken(759453, "3261063398587591693")
+addappid(759454) -- Monster Hunter World - Gesture Passionate
+addappid(759455) -- Monster Hunter World - Gesture Spin-O-Rama
+addappid(761590) -- Monster Hunter World - Gesture Air Splits
+addappid(761591) -- Monster Hunter World - Gesture Feverish Dance
+addappid(761592) -- Monster Hunter World - Gesture Gallivanting Dance
+addappid(761593) -- Monster Hunter World - Gesture Interpretive Dance
+addappid(761594) -- Monster Hunter World - Gesture Play Possum
+addappid(761595) -- Monster Hunter World - Gesture Kowtow
+addappid(761596) -- Monster Hunter World - Gesture Sleep
+addappid(761597) -- Monster Hunter World - Gesture Kneel
+addappid(761598) -- Monster Hunter World - Classic Gesture Dance
+addtoken(761598, "2898845548066732373")
+addappid(761599) -- Monster Hunter World - Classic Gesture Prance
+addtoken(761599, "10689114943303670656")
+addappid(762170) -- Monster Hunter World - Classic Gesture Rant
+addtoken(762170, "4454645109991915799")
+addappid(762171) -- Monster Hunter World - Classic Gesture Clap
+addtoken(762171, "3381164888051711569")
+addappid(762174) -- Monster Hunter World - Gesture Devil May Cry Dual Guns
+addappid(762176) -- Monster Hunter World - Gesture Spirit Fingers
+addappid(762177) -- Monster Hunter World - Gesture Windmill Whirlwind
+addappid(762178) -- Monster Hunter World - Gesture Disco Fever
+addappid(762179) -- Monster Hunter World - Gesture Squat Day
+addappid(762910) -- Monster Hunter World - Sticker Set MH All-Stars Set
+addtoken(762910, "13193558585082592365")
+addappid(762911) -- Monster Hunter World - Sticker Set Sir Loin Set
+addtoken(762911, "8943040469927994625")
+addappid(762912) -- Monster Hunter World - Sticker Set Poogie Set
+addappid(762913) -- Monster Hunter World - Sticker Set Guild Lasses Set
+addappid(762914) -- Monster Hunter World - Sticker Set Endemic Life Set
+addappid(762915) -- Monster Hunter World - Sticker Set Classic Monsters Set
+addappid(762916) -- Monster Hunter World - Sticker Set Research Commission Set
+addappid(762918) -- Monster Hunter World - Sticker Set Devil May Cry Set
+addappid(762919) -- Monster Hunter World - Sticker Set Mega Man Set
+addappid(763690) -- Monster Hunter World - Face Paint Wyvern
+addtoken(763690, "11324971425634981673")
+addappid(763691) -- Monster Hunter World - Face Paint Shade Pattern
+addappid(763692) -- Monster Hunter World - Face Paint Heart Shape
+addappid(763693) -- Monster Hunter World - Face Paint Eye Shadow
+addappid(763694) -- Monster Hunter World - Hairstyle Topknot
+addtoken(763694, "13190898475555211451")
+addappid(763695) -- Monster Hunter World - Hairstyle Provisions Manager
+addappid(763696) -- Monster Hunter World - Hairstyle Field Team Leader
+addappid(763697) -- Monster Hunter World - Hairstyle The Handler
+addappid(763698) -- Monster Hunter World - Hairstyle The Admiral
+addappid(763699) -- Monster Hunter World - The Handlers Guildmarm Costume
+addappid(770970) -- Monster Hunter World - The Handlers Astera 3 Star Chef Coat
+addappid(770972) -- Monster Hunter World - The Handlers Busy Bee Dress
+addappid(770973) -- Monster Hunter World - The Handlers Sunshine Pareo
+addappid(770974) -- Monster Hunter World - The Handlers Mischievous Dress
+addappid(770975) -- Monster Hunter World - The Handlers Winter Spirit Coat
+addappid(770976) -- Monster Hunter World - The Handlers Friendly Felyne Costume
+addappid(770977) -- Monster Hunter World - Free Character Edit Voucher
+addappid(861870) -- Monster Hunter World - Character Edit Voucher Single Voucher
+addappid(861871) -- Monster Hunter World - Character Edit Voucher Two-Voucher Pack
+addappid(861872) -- Monster Hunter World - Character Edit Voucher Three-Voucher Pack
+addappid(891730) -- Monster Hunter World - Deluxe Kit
+addappid(896580) -- Monster Hunter World - Additional Gesture Bundle 1
+addappid(974300) -- Monster Hunter World - Gesture Hip Hop Dance
+addappid(974301) -- Monster Hunter World - Gesture Cool Dance
+addappid(974302) -- Monster Hunter World - Free Sticker Set Mingle Hunter
+addappid(996720) -- Monster Hunter World - Gesture Pop Star Dance
+addappid(996721) -- Monster Hunter World - Gesture Step Dance
+addappid(996740) -- Monster Hunter World - Free Gesture Happy Hunting
+addappid(996741) -- Monster Hunter World - Sticker Set Celestial Pursuit Girls
