a9ebd6983a2d4cefc945c8c55fde5e36e174aa57
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-09-19 23:06:35 +0800

    Add files via upload

---
+++ 1020790.lua
@@ -0,0 +1,42 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1020790 Manifest
+-- Name: NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS
+-- Generated: 2025-06-06 13:04:06
+-- Total Depots: 5
+-- Total DLCs: 13
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1020790) -- NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS
+
+-- MAIN APP DEPOTS
+addappid(1020791, 1, "20b22cbbc47231bd29023b51d8a6fa2c019ad774840d245064799912ea48b105") -- Main Game Content (Windows Content)
+setManifestid(1020791, "1061767113913208859", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+addappid(229003, 1, "8e0ded6e1d9481eb286217322054ce7b0b8243daae770f31a30667752b1a3c63") -- .NET 4.0 Client Profile Redist (Shared from App 228980)
+setManifestid(229003, "8740933542064151477", 0)
+
+-- DLCS WITH DEDICATED DEPOTS
+-- NBUNSC - Nostalgic Anime Song  Item Pack (AppID: 2542820)
+addappid(2542820)
+addappid(2542820, 1, "290c05f9538952fbafab1381ad8516cb900a835310db3701bd21461afa343090") -- NBUNSC - Nostalgic Anime Song  Item Pack - Main Content
+setManifestid(2542820, "2745812014487487415", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2171320) -- NBUNSC - Costume  Accessory Pack
+addappid(2171321) -- NBUNSC - Pre-Order Pack
+addappid(2171324) -- NBUNSC - Kakashi Hatake (Maskless)
+addappid(2171325) -- NBUNSC - Naruto Uzumaki (The Great Ninja War End)
+addappid(2171326) -- NBUNSC -  DLC Pack 1
+addappid(2171327) -- NBUNSC - DLC Pack 2
+addappid(2171328) -- NBUNSC - DLC Pack 3
+addappid(2171329) -- NBUNSC - DLC Pack 4
+addappid(2171330) -- NBUNSC - DLC Pack 5
+addappid(2171332) -- NARUTO X BORUTO Ultimate Ninja STORM CONNECTIONS - Season Pass
+addappid(2594760) -- NARUTO X BORUTO Ultimate Ninja Storm Connections - Ultimate Edition Upgrade Pack
+addappid(2674900) -- Asics x Naruto Shippuden Special Sneakers
