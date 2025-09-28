e5b28cb5384405cc7c1ee031eb2fa4af1fafd58c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-09-28 09:32:50 +0800

    Add files via upload

---
+++ 1259420.lua
@@ -0,0 +1,23 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games!
+-- Steam App 1259420 Manifest
+-- Name: Days Gone
+-- Generated: 2025-07-22 09:53:49
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1259420) -- Days Gone
+
+-- MAIN APP DEPOTS
+addappid(1259421, 1, "3a92a76d80b16038abe5b2831f554a22120535ab039dbde663a70c8b52afe11b") -- Days Gone : Multi Region
+setManifestid(1259421, "9008749683399927967", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3238470) -- Days Gone - Broken Road DLC
