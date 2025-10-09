0b95cb0162bba0c57f54d93b77dd29c8e1489b08
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-09 08:33:52 +0800

    Add files via upload

---
+++ 1200770.lua
@@ -0,0 +1,22 @@
+-- 1200770's Lua and Manifest Created by Morrenus
+-- Deathground
+-- Created: October 07, 2025 at 19:20:35 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 5
+-- Total DLCs: 0
+-- Shared Depots: 4
+
+-- MAIN APPLICATION
+addappid(1200770) -- Deathground
+-- MAIN APP DEPOTS
+addappid(1200771, 1, "e5765b9e9753b6845acf30d81995371d101ac52f412ad63148fbce1a52057fa2") -- Depot 1200771
+setManifestid(1200771, "3182497870687183157", 26518087517)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
