7ec8b2b99ee029275117d8510bd5bc18cb7ed31a
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-09 19:30:41 +0800

    Add files via upload

---
+++ 1622910.lua
@@ -0,0 +1,30 @@
+-- 1622910's Lua and Manifest Created by Morrenus
+-- Still Wakes the Deep
+-- Created: September 30, 2025 at 20:09:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 8
+-- Total DLCs: 1
+-- Shared Depots: 7
+
+-- MAIN APPLICATION
+addappid(1622910) -- Still Wakes the Deep
+-- MAIN APP DEPOTS
+addappid(1622911, 1, "b826831ce8370fa43a4d194c301e6617bb1f1b05648b374a4a8896a6556dfbb2") -- Depot 1622911
+setManifestid(1622911, "8952851089369926879", 14493053067)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229000, 1, "3aae9271fb93da5395ffbfe338d3647de5dcfab3fc6fb901ca2ec28849b3d6bf") -- .NET 3.5 Redist (Shared from App 228980)
+setManifestid(229000, "4622705914179893434", 242743889)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229006, 1, "9bc3e05ce55153e5c315cb18024602ca15958a7be03adc5ffbe53b00a8524416") -- .NET 4.7 Redist (Shared from App 228980)
+setManifestid(229006, "1784011429307107530", 83944258)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3465690) -- Still Wakes the Deep Sirens Rest
