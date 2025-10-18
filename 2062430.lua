6937b54216e403e2326eff1953bd0cf73239c663
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-18 22:20:44 +0800

    Add files via upload

---
+++ 2062430.lua
@@ -0,0 +1,12 @@
+-- 2062430's Lua and Manifest Created by Morrenus
+-- BALL x PIT
+-- Created: October 15, 2025 at 18:30:04 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2062430) -- BALL x PIT
+-- MAIN APP DEPOTS
+addappid(2062431, 1, "5d28ee4226eb2240f32250a615d7cdf1e5beebec25073151597d734fc48d547c") -- Depot 2062431
+setManifestid(2062431, "6521078632916959556", 571479863)
