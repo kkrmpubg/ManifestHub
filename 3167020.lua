184f01b4e14a1f5fa673d82be7d0c205b42404e3
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-23 15:32:23 +0800

    Add files via upload

---
+++ 3167020.lua
@@ -0,0 +1,14 @@
+-- 3167020's Lua and Manifest Created by Morrenus
+-- Escape from Duckov
+-- Created: October 23, 2025 at 02:58:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3167020, 1, "868bad77591803956d03d9863f4775a7d402330362cbd41c1c764c994dfae9fa") -- Escape from Duckov
+-- MAIN APP DEPOTS
+addappid(3167021, 1, "6a1377569bff7441f985074b6ed8692c0c8f6681b80a75e020dd1a10419b5dea") -- Depot 3167021
+setManifestid(3167021, "776515690643303057", 2264720155)
+addappid(3167022, 1, "862fd0e5628b2423c67e71daa4e95147265f746e4f4adf57350eb171f822c616") -- Depot 3167022
+setManifestid(3167022, "2189816758559323339", 2275274002)
