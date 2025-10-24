679961a985fbe840c307c8a5ab05d54386bc20ef
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-24 17:42:19 +0800

    Add files via upload

---
+++ 2968420.lua
@@ -0,0 +1,12 @@
+-- 2968420's Lua and Manifest Created by Morrenus
+-- PowerWash Simulator 2
+-- Created: October 23, 2025 at 12:26:01 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2968420) -- PowerWash Simulator 2
+-- MAIN APP DEPOTS
+addappid(2968422, 1, "2c13dcc1b498282fe021b2cd49229d7309ace370d9e990cd3ad843397cf247c9") -- Depot 2968422
+setManifestid(2968422, "4943109005327745514", 8811531832)
