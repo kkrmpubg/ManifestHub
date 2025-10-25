527592174f8b01895b194392d79f1421e1c664a0
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-25 13:41:57 +0800

    Add files via upload

---
+++ 1449110.lua
@@ -0,0 +1,25 @@
+-- 1449110's Lua and Manifest Created by Morrenus
+-- The Outer Worlds 2
+-- Created: October 24, 2025 at 13:22:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 3
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1449110) -- The Outer Worlds 2
+-- MAIN APP DEPOTS
+addappid(1449111, 1, "1691760a5a08bde773f0880b6741283070c2c3931c41a09cab6682e29171c563") -- Depot 1449111
+setManifestid(1449111, "8925567079102516634", 82397979251)
+addappid(3729850, 1, "5de7b19426b9195dd06c2774cb6432657fb1a818723432602636beb88dce3dd4") -- Depot 3729850
+setManifestid(3729850, "7345451025778384948", 7934669657)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3729910) -- The Outer Worlds 2 - Commander Zanes Battle Pack
+addtoken(3729910, "7170877342749329240")
+addappid(3938110) -- The Outer Worlds 2 Pet Tie-in
+addappid(4055350) -- The Outer Worlds 2 Pet Tie-in 2
