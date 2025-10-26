79a5b4e5e712343957f020d03e0d84fe34d42240
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-26 17:32:52 +0800

    Add files via upload

---
+++ 2918500.lua
@@ -0,0 +1,12 @@
+-- 2918500's Lua and Manifest Created by Morrenus
+-- Cozy Island
+-- Created: October 19, 2025 at 16:24:06 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(2918500) -- Cozy Island
+-- MAIN APP DEPOTS
+addappid(2918501, 1, "444ce90f8665148c49ad7c254f7729d6e85e823d967a915c0e2350f887d1245f") -- Depot 2918501
+setManifestid(2918501, "4364611307963802198", 895588046)
