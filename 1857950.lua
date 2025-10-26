fefad9f6a6591a3b21b24c99da44b31d8b158ca3
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-27 04:58:36 +0800

    Add files via upload

---
+++ 1857950.lua
@@ -0,0 +1,20 @@
+-- 1857950's Lua and Manifest Created by Morrenus
+-- VEIN
+-- Created: October 25, 2025 at 21:02:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1857950) -- VEIN
+-- MAIN APP DEPOTS
+addappid(1857952, 1, "0f09efc4c8566ce587f3329874cc02f750a58ad24fa6d30c8154b1b95cf2f050") -- Depot 1857952
+setManifestid(1857952, "2946550909401493340", 62903571396)
+addappid(1857951, 1, "e414cf1e5b3b3a2034a90ccb1b067f244b73e91fd8fe331fde0149260b69cb62") -- Depot 1857951
+setManifestid(1857951, "7283121679164505852", 56120621454)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
