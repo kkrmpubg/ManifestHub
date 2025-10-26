8ddd4ae67ff102f7b416e7f89c8fee058e0597ec
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-27 04:50:07 +0800

    Add files via upload

---
+++ 3573430.lua
@@ -0,0 +1,12 @@
+-- 3573430's Lua and Manifest Created by Morrenus
+-- Cargo Simulator 25
+-- Created: October 26, 2025 at 12:18:49 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3573430) -- Cargo Simulator 25
+-- MAIN APP DEPOTS
+addappid(3573431, 1, "b52e20b6537b345c0a243cdd553295a6cd467707056827e49e229532aa1eeb19") -- Depot 3573431
+setManifestid(3573431, "1474542317695130648", 1410525594)
