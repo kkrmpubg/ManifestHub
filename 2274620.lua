028ae56d5c54f06c81c7bbb6b9cc878da4477991
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-26 17:53:59 +0800

    Add files via upload

---
+++ 2274620.lua
@@ -0,0 +1,17 @@
+-- 2274620's Lua and Manifest Created by Morrenus
+-- Discounty
+-- Created: October 02, 2025 at 10:39:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 1
+
+-- MAIN APPLICATION
+addappid(2274620) -- Discounty
+-- MAIN APP DEPOTS
+addappid(2274621, 1, "d80dd2f7740fe455053082e62f1e36d65a4106f01468400177459d688063b8bc") -- Depot 2274621
+setManifestid(2274621, "3970336220278392336", 647841805)
+-- DLCS WITH DEDICATED DEPOTS
+-- Discounty - Supporter Pack (AppID: 3903750)
+addappid(3903750)
+addappid(3903750, 1, "eec2964a88dcbbdbbdee1d7f4be6c44ffe7fcbd3f316daf3b0854378b30e989a") -- Discounty - Supporter Pack - Depot 3903750
+setManifestid(3903750, "3642238952312024190", 72)
