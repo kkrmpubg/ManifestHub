7c645ee2dcb5e0eea1267513862ebfbd23506cda
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-29 14:04:09 +0800

    Add files via upload

---
+++ 2428980.lua
@@ -0,0 +1,16 @@
+-- 2428980's Lua and Manifest Created by Morrenus
+-- Bye Sweet Carole
+-- Created: October 22, 2025 at 05:33:05 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2428980) -- Bye Sweet Carole
+-- MAIN APP DEPOTS
+addappid(2428983, 1, "4459aa4e531d8007450e4a278ffab68b61d229d38bc1801993ed63bd4275d455") -- Depot 2428983
+setManifestid(2428983, "6195847462009990827", 15643446181)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Bye Sweet Carole - Artbook (AppID: 3902770) - missing depot keys
+-- addappid(3902770)
