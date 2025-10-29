f1f082ac787b9e1b5f57e98129cc2fd6846a25e4
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-29 14:05:44 +0800

    Add files via upload

---
+++ 2891020.lua
@@ -0,0 +1,16 @@
+-- 2891020's Lua and Manifest Created by Morrenus
+-- Undusted: Letters from the Past
+-- Created: October 25, 2025 at 06:15:29 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0 (1 excluded)
+
+-- MAIN APPLICATION
+addappid(2891020) -- Undusted: Letters from the Past
+-- MAIN APP DEPOTS
+addappid(2891021, 1, "a90ed813e052210c9fcfb1c1b5198e4859120ad61da4afeeba57383d1829cdc0") -- Depot 2891021
+setManifestid(2891021, "4803625571905051026", 1401552492)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Undusted Letters from the Past Artbook (AppID: 3904790) - missing depot keys
+-- addappid(3904790)
