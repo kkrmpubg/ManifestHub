42cc0387767d10566b864651d65518a18e6ea8f2
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-31 01:53:22 +0800

    Add files via upload

---
+++ 1541370.lua
@@ -0,0 +1,12 @@
+-- 1541370's Lua and Manifest Created by Morrenus
+-- Ultimate Theater Simulator
+-- Created: October 30, 2025 at 10:28:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1541370) -- Ultimate Theater Simulator
+-- MAIN APP DEPOTS
+addappid(1541373, 1, "b4a071c496a6629cb37894cf11490e438337b38ba5b8f139b1ed46a7db7a58c3") -- Depot 1541373
+setManifestid(1541373, "7142113194445485400", 1930632780)
