2a805603393d1690286814308ddecfbf7b7887de
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-10-31 13:30:22 +0800

    Add files via upload

---
+++ 2246670.lua
@@ -0,0 +1,49 @@
+-- 2246670's Lua and Manifest Created by Morrenus
+-- Tales of Xillia Remastered
+-- Created: October 31, 2025 at 00:13:13 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 5 (6 excluded)
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(2246670) -- Tales of Xillia Remastered
+-- MAIN APP DEPOTS
+addappid(2246671, 1, "e144ac4eb76015ab095c549730d07519d9ae2916c28c7f6940bfd7a0d8eb6b35") -- Depot 2246671
+setManifestid(2246671, "5470154190148824852", 18271593841)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITH DEDICATED DEPOTS
+-- Tales of Xillia Remastered - Digital Artbook  Soundtrack (AppID: 2833370)
+addappid(2833370)
+addappid(2833370, 1, "d5179861da87edc0542c6a0fe202ff8051045e81972da7091abb36632c215255") -- Tales of Xillia Remastered - Digital Artbook  Soundtrack - Depot 2833370
+setManifestid(2833370, "3906636434351247684", 925145538)
+-- Tales of Xillia Remastered - Super Adventuring Assistance Set (AppID: 3106420)
+addappid(3106420)
+addappid(3106420, 1, "cd0a4844be9f46cd490eb1d54db804db59255318c2e80366ffcdc1b848cbd01c") -- Tales of Xillia Remastered - Super Adventuring Assistance Set - Depot 3106420
+setManifestid(3106420, "4033023325803216249", 4491)
+-- Tales of Xillia Remastered - Super Growth Support Herb Set (AppID: 3106430)
+addappid(3106430)
+addappid(3106430, 1, "fb30f9c22a8920e904b9cf8f07f220e453cc576b8db97d1e751220329b617b4f") -- Tales of Xillia Remastered - Super Growth Support Herb Set - Depot 3106430
+setManifestid(3106430, "8410528672328740297", 4529)
+-- Tales of Xillia Remastered - Battle BGM Pack (AppID: 3106440)
+addappid(3106440)
+addappid(3106440, 1, "1af6fd9f43a86c1c34dc0b5d3974cd214bf31f30aa4de2a3797f46875b0a1ba7") -- Tales of Xillia Remastered - Battle BGM Pack - Depot 3106440
+setManifestid(3106440, "1142284471696305737", 118592834)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3106450) -- Tales of Xillia Remastered - Deluxe Upgrade Pack
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Tales of Xillia Remastered - Lv. Up5 (1) (AppID: 3106360) - missing depot keys
+-- addappid(3106360)
+-- Tales of Xillia Remastered - Lv. Up5 (2) (AppID: 3106370) - missing depot keys
+-- addappid(3106370)
+-- Tales of Xillia Remastered - Lv. Up5 (3) (AppID: 3106380) - missing depot keys
+-- addappid(3106380)
+-- Tales of Xillia Remastered - Lv. Up5 (4) (AppID: 3106390) - missing depot keys
+-- addappid(3106390)
+-- Tales of Xillia Remastered - Lv. Up10 (1) (AppID: 3106400) - missing depot keys
+-- addappid(3106400)
+-- Tales of Xillia Remastered - Lv. Up10 (2) (AppID: 3106410) - missing depot keys
+-- addappid(3106410)
