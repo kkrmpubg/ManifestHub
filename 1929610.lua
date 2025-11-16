df07b1c731ca25503c49d9eb9abeb1e27f9bfd4d
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-16 19:35:38 +0800

    Add files via upload

---
+++ 1929610.lua
@@ -0,0 +1,18 @@
+-- 1929610's Lua and Manifest Created by Morrenus
+-- Demonologist
+-- Created: September 29, 2025 at 07:21:52 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1929610) -- Demonologist
+-- MAIN APP DEPOTS
+addappid(1929611, 1, "17c7cca3b4b0c06f525fe2745e8d2ff64f80b66ca39321e251d0a89c66dd6164") -- Depot 1929611
+setManifestid(1929611, "5382708548440948058", 30534834144)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
