1f8ac2c7f1e10fd1d57bd1dfed2fc7b0f3f6af4f
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-16 19:52:02 +0800

    Add files via upload

---
+++ 1609370.lua
@@ -0,0 +1,22 @@
+-- 1609370's Lua and Manifest Created by Morrenus
+-- Caravanners: Co-op Open World Camping
+-- Created: November 10, 2025 at 03:41:22 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0 (1 excluded)
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1609370, 1, "636c9af1caa16f34922f7c860a4e8af0268b9b4e8e734f24cd2f00f358f3965f") -- Caravanners: Co-op Open World Camping
+-- MAIN APP DEPOTS
+addappid(1609371, 1, "e4f9fb9d3d3c5cb6a3b8ccbf7696c64e7cfc8b2aa22c525648e9344ed8d3d3a7") -- Depot 1609371
+setManifestid(1609371, "4935700408356360885", 19023707074)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Founders Limited Edition Pack (AppID: 3755710) - missing depot keys
+-- addappid(3755710)
