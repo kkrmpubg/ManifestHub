1e2061ed02bd8f7e3e4c40607a505031020eff0f
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-16 19:53:16 +0800

    Add files via upload

---
+++ 1062520.lua
@@ -0,0 +1,12 @@
+-- 1062520's Lua and Manifest Created by Morrenus
+-- Dinkum
+-- Created: November 08, 2025 at 00:38:11 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1062520) -- Dinkum
+-- MAIN APP DEPOTS
+addappid(1062521, 1, "ef0a62caf543b86609cfc0a77c6879d1f131a1fdb5cfdda01204f0171b126a56") -- Depot 1062521
+setManifestid(1062521, "4645239552839685", 3087066026)
