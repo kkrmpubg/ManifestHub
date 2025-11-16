04078e6ed14b7a5b3120815ccf6fb31924294c6f
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-16 19:48:35 +0800

    Add files via upload

---
+++ 3164500.lua
@@ -0,0 +1,12 @@
+-- 3164500's Lua and Manifest Created by Morrenus
+-- Schedule I
+-- Created: November 04, 2025 at 18:23:20 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3164500) -- Schedule I
+-- MAIN APP DEPOTS
+addappid(3164501, 1, "81d0de07788de885787b0519b9dabf51618be6cba9124497decf572ac96624f4") -- Depot 3164501
+setManifestid(3164501, "7290691196745256540", 7725687390)
