8965da432fe592e8f41d22f56df3087a745ee65b
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-16 19:46:25 +0800

    Add files via upload

---
+++ 1288320.lua
@@ -0,0 +1,35 @@
+-- 1288320's Lua and Manifest Created by Morrenus
+-- Way of the Hunter
+-- Created: October 30, 2025 at 17:44:25 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 16
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1288320) -- Way of the Hunter
+-- MAIN APP DEPOTS
+addappid(1288321, 1, "708ac4e2fd466252d4859b1e41672d67d6ae8d17be1285cc01f0e3517dbc1dac") -- Depot 1288321
+setManifestid(1288321, "8367755166778009974", 24818344687)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2012180) -- Way of the Hunter - Map Pack 1
+addappid(2014490) -- Way of the Hunter - Hunters Pack
+addappid(2054470) -- Way of the Hunter - Aurora Shores
+addappid(2054471) -- Way of the Hunter - Tikamoon Plains
+addappid(2211610) -- Way of the Hunter - Free UTV
+addappid(2584760) -- Way of the Hunter - Steyr Arms Pack
+addappid(2698430) -- Way of the Hunter - Matariki Park
+addappid(2793490) -- Way of the Hunter - Map Pack 2
+addappid(2922200) -- Way of the Hunter - Outfits Pack
+addappid(2922210) -- Way of the Hunter - Kawasaki UTV Pack
+addappid(2922220) -- Way of the Hunter - Remington Firearms Pack
+addappid(2922230) -- Way of the Hunter - Hunters Advantage Pack
+addappid(3091570) -- Way of the Hunter - Lintukoto Reserve
+addappid(3091580) -- Way of the Hunter - Bear Archery Pack
+addappid(3760210) -- Way of the Hunter - Night Hunting Pack
+addappid(4071720) -- Way of the Hunter - Christensen Arms Pack
