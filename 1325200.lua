e26eed75de5a7baa273f832f0b2b431347d65bd8
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-17 16:18:10 +0800

    Add files via upload

---
+++ 1325200.lua
@@ -0,0 +1,22 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1325200 Manifest
+-- Name: Nioh 2 – The Complete Edition
+-- Generated: 2025-06-06 14:24:12
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1325200) -- Nioh 2 – The Complete Edition
+
+-- MAIN APP DEPOTS
+addappid(1325201, 1, "20da81589e0a73461a18d3f42426068e48228b81ea86664c8b93ffdf7285762a") -- Nioh2 Common Data
+setManifestid(1325201, "114676460224695371", 0)
+addappid(1325202, 1, "e644223a7b80d05032b8d93def9d0473b80c7430da7e55e3540eefbf0961d0e1") -- Nioh2 Application
+setManifestid(1325202, "3674821973469504854", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
