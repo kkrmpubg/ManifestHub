a40584603306f49e4387e2dcd8c5fe12fad96627
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-22 17:45:08 +0800

    Add files via upload

---
+++ 2625420.lua
@@ -0,0 +1,18 @@
+-- 2625420's Lua and Manifest Created by Morrenus
+-- Drive Beyond Horizons
+-- Created: November 01, 2025 at 07:45:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2625420) -- Drive Beyond Horizons
+-- MAIN APP DEPOTS
+addappid(2625421, 1, "e6632a8ac0813239fe1784858878c1dc48c48075d55d2b4045f529ba7ba3a01c") -- Depot 2625421
+setManifestid(2625421, "3213920384131834323", 11584337847)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
