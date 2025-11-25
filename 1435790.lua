c9c28a0fbfcf76c1f8df63eba3975b69ef3f4105
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-25 17:40:58 +0800

    Add files via upload

---
+++ 1435790.lua
@@ -0,0 +1,89 @@
+-- 1435790's Lua and Manifest Created by Morrenus
+-- Escape Simulator
+-- Created: October 03, 2025 at 03:40:18 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 29
+-- Total DLCs: 10
+
+-- MAIN APPLICATION
+addappid(1435790, 1, "dc4fc1a6086c3fec14fa8c15648fb902bdece70b871d7cca5a69506011b30f0c") -- Escape Simulator
+-- MAIN APP DEPOTS
+addappid(1435791, 1, "4057c2e4d3083f052c881b031171988306f2bde449c77ab6cfd5541947857170") -- Escape Simulator Windows
+setManifestid(1435791, "7764755463407141867", 28689052238)
+addappid(1435792, 1, "8b3951f256821087d5326a05a2cf23f36379373dec625c63ec7f4c7cdbdb13a0") -- Escape Simulator Mac
+setManifestid(1435792, "2720954515231385141", 28832759500)
+addappid(1435793, 1, "d04510be8de95b7ea005909ec5ea68132911a2111a19da874f06676b970fe273") -- Escape Simulator Linux
+setManifestid(1435793, "6705031198727187877", 32633675118)
+-- DLCS WITH DEDICATED DEPOTS
+-- Escape Simulator Steampunk DLC (AppID: 1942100)
+addappid(1942100)
+addappid(1942101, 1, "94bbdab36b8a43027b43ef0ba68f9f65364dad25e65ae6cef303120421471d52") -- Escape Simulator Steampunk DLC - Depot 1942101
+setManifestid(1942101, "9138071753484625088", 1989651987)
+addappid(1942102, 1, "db33217a845d1ff6a94526b18c5594f0749bc6db1db16d2561cbdaebcb32610d") -- Escape Simulator Steampunk DLC - Depot 1942102
+setManifestid(1942102, "6311021139214740618", 2004183917)
+addappid(1942103, 1, "221a7c112dc519ce94404f8df55a834fdbcae272f816efe386e3bfc4897d31bd") -- Escape Simulator Steampunk DLC - Depot 1942103
+setManifestid(1942103, "748118071510730580", 2433673089)
+-- Escape Simulator Portal DLC (AppID: 2000170)
+addappid(2000170)
+addappid(2000171, 1, "6fd96776a4f0d78d1a785948c00a3370ac28152ddb9dfb900eb12f445edb569d") -- Escape Simulator Portal DLC - Depot 2000171
+setManifestid(2000171, "544960132068981305", 1030283555)
+addappid(2000172, 1, "3a6668a7a6dc6d1cb5e4ca343ba7a351ce715eaaaacb3921d512fc459506bc4d") -- Escape Simulator Portal DLC - Depot 2000172
+setManifestid(2000172, "7822348840246250794", 1034548357)
+addappid(2000173, 1, "3e99148664ebfc54b2115a527ec7f0a788a617cbdf9be34a06ee494f7e815926") -- Escape Simulator Portal DLC - Depot 2000173
+setManifestid(2000173, "1479351674591888670", 1127590405)
+-- Escape Simulator Wild West DLC (AppID: 2175260)
+addappid(2175260)
+addappid(2175261, 1, "de314519da801ba0df61d5dbeebaa2a4b681c21e55f4f90df3172608579740f2") -- Escape Simulator Wild West DLC - Depot 2175261
+setManifestid(2175261, "4554297078629761298", 1909309023)
+addappid(2175262, 1, "88ed3d5073016506f4008c64c5a2249622f3c2317cf109a43313af649d2fb7a0") -- Escape Simulator Wild West DLC - Depot 2175262
+setManifestid(2175262, "5553074800641849326", 1923173944)
+addappid(2175263, 1, "b2067039860ecfbbef633b648fb29a7f5438e94f1a4f7770fa4e272249daf43a") -- Escape Simulator Wild West DLC - Depot 2175263
+setManifestid(2175263, "7234441360137336478", 2382138130)
+-- Escape Simulator Magic DLC (AppID: 2419810)
+addappid(2419810)
+addappid(2419811, 1, "38e760cda6c0e0a518b9d4f01513b48cd3860150d6a850e6b73ebd68dad642bb") -- Escape Simulator Magic DLC - Depot 2419811
+setManifestid(2419811, "7125610067362755743", 3108231515)
+addappid(2419812, 1, "2dd31bc062412c20887664b718a748b8a23b96dc56411aff87809afa74045c7d") -- Escape Simulator Magic DLC - Depot 2419812
+setManifestid(2419812, "8769434164710600780", 3125900984)
+addappid(2419813, 1, "64fc8e30814b1163287fbcb311b54a7579eccb77a3141fb30a90a3437f7f3864") -- Escape Simulator Magic DLC - Depot 2419813
+setManifestid(2419813, "2877109868819754643", 3654840393)
+-- Escape Simulator Among Us DLC (AppID: 2644870)
+addappid(2644870)
+addappid(2644871, 1, "8db0e32271fe81dc31e2181673a06a52dceaa00e7ed671e8bd301c8aae61b597") -- Escape Simulator Among Us DLC - Depot 2644871
+setManifestid(2644871, "1273970021864218249", 370379377)
+addappid(2644872, 1, "f032cb22636e7bc7db98c9a88485a179121feef3e093809d1e2233815a9b0f0b") -- Escape Simulator Among Us DLC - Depot 2644872
+setManifestid(2644872, "5841063230196481757", 372312370)
+addappid(2644873, 1, "cc56d0e0e0bd69367bd8d6a9dea798b5711718e0684d3a21e1f3a3a324fb72ca") -- Escape Simulator Among Us DLC - Depot 2644873
+setManifestid(2644873, "5118428635174920475", 424345650)
+-- Escape Simulator Mayan DLC (AppID: 2644890)
+addappid(2644890)
+addappid(2644891, 1, "cf3c2c613e9f7a37e6270a585eb2b4adc94756b12b38d63917bf6c317b5c142a") -- Escape Simulator Mayan DLC - Depot 2644891
+setManifestid(2644891, "7243949069247745797", 1525193002)
+addappid(2644892, 1, "080e0d23f2b479583a982b399098e016729270b1d89c48856da3fabd40f9b814") -- Escape Simulator Mayan DLC - Depot 2644892
+setManifestid(2644892, "1876460747319217344", 1547183980)
+addappid(2644893, 1, "b6704f28a017405e2e6419d42727d104d8645285f15ca501b6fa184254b333b2") -- Escape Simulator Mayan DLC - Depot 2644893
+setManifestid(2644893, "1922692175112638791", 2140246058)
+-- Escape Simulator Supporter Pack DLC (AppID: 2843390)
+addappid(2843390)
+addappid(2843391, 1, "3c4de5a39a2a16aab580fdaf965410fea8c7cfd3ac4abbcb5b0615f4dfe87479") -- Escape Simulator Supporter Pack DLC - Depot 2843391
+setManifestid(2843391, "7258005221969580784", 312714496)
+-- Escape Simulator PowerWash DLC (AppID: 2879620)
+addappid(2879620)
+addappid(2879621, 1, "98e8839ac92ed3f458755d7ed994b25b3f152d1b665c72b088468eb2f5ce0d71") -- Escape Simulator PowerWash DLC - Depot 2879621
+setManifestid(2879621, "1243717326532108995", 391377796)
+addappid(2879622, 1, "b26e18d223fad8dde74ad4e1f996f896b50a813b764d563066f69e79f111987b") -- Escape Simulator PowerWash DLC - Depot 2879622
+setManifestid(2879622, "1914535262038615556", 394094132)
+addappid(2879623, 1, "c5c28e20f89f33b30448a228ea85aa2533a9c313bfddcb74bdf2777775683c4e") -- Escape Simulator PowerWash DLC - Depot 2879623
+setManifestid(2879623, "5717225719696203399", 490071832)
+-- Escape Simulator Talos DLC (AppID: 3193660)
+addappid(3193660)
+addappid(3193661, 1, "bf40c73a0e77967ddc30859ca94aa4e4d2b7da47f132fe9ef5b403ea6c8f8239") -- Escape Simulator Talos DLC - Depot 3193661
+setManifestid(3193661, "4760553673854196860", 574613523)
+addappid(3193662, 1, "f05e1412e708a34e97be6aa14db6ac2402c42cf9dd838f0545ded77a1581cb5a") -- Escape Simulator Talos DLC - Depot 3193662
+setManifestid(3193662, "7727191578338045427", 581101235)
+addappid(3193663, 1, "455b6c7e6a58f501d72f19cbc64ede33686950062b1afacba974e16056f2894f") -- Escape Simulator Talos DLC - Depot 3193663
+setManifestid(3193663, "8053838337269895168", 755896802)
+-- Escape Simulator Spy DLC (AppID: 3972570)
+addappid(3972570)
+addappid(3972571, 1, "bacc08147e046c64e1b55a5f84bca359f194493d9cedfb954ffd8644d6f0a704") -- Escape Simulator Spy DLC - Depot 3972571
+setManifestid(3972571, "7240178671330418625", 2275139755)
