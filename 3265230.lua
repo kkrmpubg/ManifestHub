5998754987376d6c6c7e11cf0a3bdeb54186d58d
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-28 22:03:30 +0800

    Add files via upload

---
+++ 3265230.lua
@@ -0,0 +1,12 @@
+-- 3265230's Lua and Manifest Created by Morrenus
+-- Cursed Companions
+-- Created: November 24, 2025 at 17:37:06 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3265230) -- Cursed Companions
+-- MAIN APP DEPOTS
+addappid(3265231, 1, "11ad2b44470571d638367adae842d8936671420772e9e849afdffb78a6580f02") -- Depot 3265231
+setManifestid(3265231, "4124361625832912987", 4434057552)
