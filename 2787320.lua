e21ff183385bb7e9599b9cd3696fb5595931b4c6
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-11-30 18:50:50 +0800

    Add files via upload

--- 2787320.lua
+++ 2787320.lua
@@ -1,31 +1,7 @@
--- 2787320's Lua and Manifest Created by Morrenus
--- Revenge of the Savage Planet
--- Created: November 29, 2025 at 18:08:23 EST
--- Website: https://manifest.morrenus.xyz/
--- Total Depots: 4
--- Total DLCs: 8
--- Shared Depots: 2
-
--- MAIN APPLICATION
-addappid(2787320) -- Revenge of the Savage Planet
--- MAIN APP DEPOTS
-addappid(2787321, 1, "07fdf2268a8c0b6b8d7dd4c5d7978e054ce837759193f7f5cacd2968150199a7") -- Depot 2787321
-setManifestid(2787321, "6031702051004731226", 15167934892)
--- SHARED DEPOTS (from other apps)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "3514306556860204959", 39590283)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 102931551)
--- DLCS WITH DEDICATED DEPOTS
--- Revenge of the Savage Planet - Artbook and Soundtrack In-Game (AppID: 3482310)
-addappid(3482310)
-addappid(3482311, 1, "ecf76b3f40656549d3b5e12e0ab1d6b9cfb6aabeb3836faf663b7d0008efaddb") -- Revenge of the Savage Planet - Artbook and Soundtrack In-Game - Depot 3482311
-setManifestid(3482311, "7611365059317075932", 74809596)
--- DLCS WITHOUT DEDICATED DEPOTS
-addappid(3482320) -- Revenge of the Savage Planet - Raccoon Suit
-addappid(3482330) -- Revenge of the Savage Planet - HR Enforcer Suit
-addappid(3482340) -- Revenge of the Savage Planet - Deluxe Edition Quests
-addappid(3654280) -- The Cosmic Hoarder Upgrade
-addappid(3934430) -- Revenge of the Savage Planet - Deluxe Charity Edition
-addappid(3986890) -- Revenge of the Savage Planet - Remix Mode
-addappid(4141000) -- Revenge of the Savage Planet - Bingo Brawl
+addappid(2787320)
+addappid(3482320)
+addappid(3482330)
+addappid(3482340)
+addappid(3654280)
+addappid(2787321,0,"07fdf2268a8c0b6b8d7dd4c5d7978e054ce837759193f7f5cacd2968150199a7")
+setManifestid(2787321,"3478749889352457561")
