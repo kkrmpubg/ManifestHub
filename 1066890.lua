3dba5426aaa2ce62cdf20aa53b3cdf9a4d9ed042
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-02 18:54:07 +0800

    Add files via upload

---
+++ 1066890.lua
@@ -0,0 +1,51 @@
+-- 1066890's Lua and Manifest Created by Morrenus
+-- Automobilista 2
+-- Created: November 09, 2025 at 07:19:32 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 30
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1066890, 1, "a1ecf0fa8dda29914e48d578e094cd46ba18f823468c088c586523349f68e617") -- Automobilista 2
+-- MAIN APP DEPOTS
+addappid(1066891, 1, "d6f49d09d5511c95e9e8ea45456deb337c87a07d9df1474f781f70dcf3bfae72") -- Automobilista 2 Content
+setManifestid(1066891, "2751965451552297425", 132047191318)
+-- SHARED DEPOTS (from other apps)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1261120) -- Automobilista 2 2020-2021 Season Pass
+addappid(1377650) -- Hockenheimring Pack
+addappid(1386930) -- Automobilista 2 - Silverstone Pack
+addappid(1386931) -- Automobilista 2 - Nurburgring Pack
+addappid(1386932) -- Automobilista 2 - Spa-Francorchamps Pack
+addappid(1392090) -- Automobilista 2 - Historical Track Pack Pt1
+addappid(1392091) -- Automobilista 2 - Supercars Pack Pt1
+addappid(1392100) -- Automobilista 2 Premium Track Pack
+addappid(1648060) -- Automobilista 2 - Monza Pack
+addappid(1648061) -- Automobilista 2 - Racin USA Pack Pt1
+addappid(1648062) -- Automobilista 2 - Racin USA Pack Pt2
+addappid(1648063) -- Automobilista 2 - Racin USA Pack Pt3
+addappid(1648110) -- Automobilista 2 - Racin USA Full Expansion Pack
+addappid(1655040) -- Automobilista 2 Premium Expansion Packs
+addappid(2238190) -- Automobilista 2- Brazilian Racing Legends Pack Pt1
+addappid(2461740) -- Automobilista 2 - Circuit de Barcelona-Catalunya
+addappid(2518750) -- Automobilista 2 - Adrenaline Pack Pt1
+addappid(2518760) -- Automobilista 2 - Adrenaline Pack Pt2
+addappid(2697770) -- Automobilista 2 - Historical Track Pack Pt2
+addappid(2697780) -- Automobilista 2 - Formula HiTech
+addappid(2697790) -- Automobilista 2 - Circuit des 24 Heures du Mans
+addappid(2697800) -- Automobilista 2 - Endurance Pack Pt1
+addappid(3022980) -- Automobilista 2 - IMSA Track Pack
+addappid(3022990) -- Automobilista 2 - Endurance Pack Pt2
+addappid(3023000) -- Automobilista 2 - Lamborghini Dream Pack Pt1
+addappid(3532160) -- Automobilista 2 - Endurance Pack Pt3
+addappid(3532180) -- Automobilista 2 - Lamborghini Dream Pack Pt2
+addappid(3895690) -- Automobilista 2 - Nrburgring 2025
+addappid(4044660) -- Automobilista 2 - Historical Endurance Pack Pt1
+addappid(4044670) -- Automobilista 2 - Historical Track Pack Pt3
