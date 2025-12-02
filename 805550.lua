3ff87120bda6d57a2fc87a0ee1f3cbdd1b8f33a7
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-02 17:46:31 +0800

    Add files via upload

---
+++ 805550.lua
@@ -0,0 +1,28 @@
+-- 805550's Lua and Manifest Created by Morrenus
+-- Assetto Corsa Competizione
+-- Created: October 24, 2025 at 22:21:32 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 9
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(805550) -- Assetto Corsa Competizione
+-- MAIN APP DEPOTS
+addappid(805551, 1, "6f5126fe4ab830a15b56dc7f0fca443085cf934b3a86b59bae02e77be7d1e8fe") -- Assetto Corsa Competizione Content
+setManifestid(805551, "6429394964901709272", 20255541418)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1201360) -- Assetto Corsa Competizione - Intercontinental GT Pack
+addappid(1337860) -- Assetto Corsa Competizione - GT4 Pack
+addappid(1449260) -- Assetto Corsa Competizione - 2020 GT World Challenge Pack
+addappid(1493170) -- Assetto Corsa Competizione - British GT Pack
+addappid(1865950) -- Assetto Corsa Competizione - Challengers Pack
+addappid(1865970) -- Assetto Corsa Competizione - American Track Pack
+addappid(2328720) -- Assetto Corsa Competizione - 2023 GT World Challenge Pack
+addappid(2700600) -- Assetto Corsa Competizione - GT2 Pack
+addappid(2700610) -- Assetto Corsa Competizione - 24H Nrburgring Pack
