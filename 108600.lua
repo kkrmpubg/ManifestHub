460329c37386ee80a9d25788d82962315a23d5b1
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-03 23:08:26 +0800

    Add files via upload

---
+++ 108600.lua
@@ -0,0 +1,28 @@
+-- 108600's Lua and Manifest Created by Morrenus
+-- Project Zomboid
+-- Created: September 30, 2025 at 08:13:45 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 8
+-- Total DLCs: 0
+-- Shared Depots: 5
+
+-- MAIN APPLICATION
+addappid(108600, 1, "fc06c9893d7792ec35292dfc1b216f92d0f9e173df982804f4ab00293e9ab25b") -- Project Zomboid
+-- MAIN APP DEPOTS
+addappid(108602, 1, "2c6834d3d5c0e48329883a18abe5614aa868c3e7911fc582c613cec21b3a5d1c") -- Project Zomboid Mac Depot
+setManifestid(108602, "362539403589738701", 7175738058)
+addappid(108603, 1, "0b2a31059fa239993389b69ca9a0bebd9e3cbc8ccb446335c2becbe85060b0c4") -- Project Zomboid Linux Depot
+setManifestid(108603, "6529967175871940863", 7200710660)
+addappid(108604, 1, "3f8e8c70818c5469711aeaefe50b4f280028b72dfd7f7a4c83b6bd747e4a382a") -- Project Zomboid Windows Depot
+setManifestid(108604, "8495676860137747126", 7305916760)
+-- SHARED DEPOTS (from other apps)
+addappid(228983, 1, "77c8e812cd79e67e2d376721253ebb07e06b3646f05671c6c9517b27be14734b") -- VC 2010 Redist (Shared from App 228980)
+setManifestid(228983, "8124929965194586177", 19265607)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- VC 2012 Redist (Shared from App 228980)
+setManifestid(228984, "2547553897526095397", 13742505)
+addappid(228985, 1, "21a2f639584d9bd462c5c4b3e10f881cba8a17ae674c830c4f083551cd356e9c") -- VC 2013 Redist (Shared from App 228980)
+setManifestid(228985, "3966345552745568756", 13699237)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+addappid(228987, 1, "cf0622b6dec67606fdc42e7afa5ede78cb33e38dfab82670c5ec7e1404e4984e") -- VC 2017 Redist (Shared from App 228980)
+setManifestid(228987, "4302102680580581867", 29664201)
