b5af1faf6ce3497512bab97a89220e9ca156c481
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-05 20:24:09 +0800

    Add files via upload

---
+++ 1650010.lua
@@ -0,0 +1,39 @@
+-- Generated Lua Manifest by Solus Bot in Morrenus Games
+-- Steam App 1650010 Manifest
+-- Name: RIDE 5
+-- Generated: 2025-06-07 00:28:31
+-- Total Depots: 3
+-- Total DLCs: 17
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(1650010) -- RIDE 5
+
+-- MAIN APP DEPOTS
+addappid(1650011, 1, "aeb35777de6823dd579753720a7ef2d1e1e4908fb2136a76d3325da51f7b3e0a") -- Main Game Content (Windows Content)
+setManifestid(1650011, "4534034758560317760", 0)
+
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 0)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 0)
+
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2315150) -- RIDE 5 - Far East Pack
+addappid(2315160) -- RIDE 5 - Rebel Pack
+addappid(2315170) -- RIDE 5 - Credits Multiplier
+addappid(2315180) -- RIDE 5 - Welcome Pack
+addappid(2315190) -- RIDE 5 - King of the Hills Pack
+addappid(2315200) -- RIDE 5 - Free Pack 01
+addappid(2315210) -- RIDE 5 - Short Track Pack
+addappid(2315220) -- RIDE 5 - Free Pack 02
+addappid(2315230) -- RIDE 5 - Dreamers Garage Pack
+addappid(2315240) -- RIDE 5 - Free Pack 03
+addappid(2315250) -- RIDE 5 - Racing Icons Pack
+addappid(2315260) -- RIDE 5 - Free Pack 04
+addappid(2315270) -- RIDE 5 - Born to Race Pack
+addappid(2315290) -- RIDE 5 - Free Pack 05
+addappid(2315300) -- RIDE 5 - Speed with Style Pack
+addappid(2315310) -- RIDE 5 - Free Pack 06
+addappid(2331010) -- RIDE 5 - Season Pass
