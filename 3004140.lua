8f0b835eb75f5c1628855a6e1ce83525068688bc
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-13 21:50:39 +0800

    Add files via upload

---
+++ 3004140.lua
@@ -0,0 +1,18 @@
+-- 3004140's Lua and Manifest Created by Morrenus
+-- LockYourDoor
+-- Created: December 01, 2025 at 05:54:14 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 0
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(3004140) -- LockYourDoor
+-- MAIN APP DEPOTS
+addappid(3004141, 1, "c5482a764f6c408ac116fa2d938d157c8fe9e44b6d605dbff37e9d42c42edf2c") -- Depot 3004141
+setManifestid(3004141, "4623061274173988478", 2749489810)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
