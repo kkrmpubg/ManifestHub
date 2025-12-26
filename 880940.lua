a11caee11a0319f6c72f2b22dd12a2f125c6dd7a
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-26 17:18:43 +0800

    Add files via upload

---
+++ 880940.lua
@@ -0,0 +1,16 @@
+-- 880940's Lua and Manifest Created by Morrenus
+-- Pummel Party
+-- Created: October 15, 2025 at 20:49:46 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(880940, 1, "5279674a32bc137d15796bc284c97323c365f4aabc5c5781cd337906f34367a1") -- Pummel Party
+-- MAIN APP DEPOTS
+addappid(880941, 1, "ce9aa368e895b19d481941c1bc33d93fa1b495f6046caeee282580a6eac1a99c") -- Pummel Party Content
+setManifestid(880941, "8651477077702693582", 2319355964)
+-- SHARED DEPOTS (from other apps)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
