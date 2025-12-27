b2f86201a1ef5c6ff97fb4ef2d882b1342355c4a
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-27 19:29:20 +0800

    Add files via upload

---
+++ 1284190.lua
@@ -0,0 +1,15 @@
+-- 1284190's Lua and Manifest Created by Morrenus
+-- The Planet Crafter
+-- Created: December 10, 2025 at 09:54:31 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 2
+
+-- MAIN APPLICATION
+addappid(1284190) -- The Planet Crafter
+-- MAIN APP DEPOTS
+addappid(1284192, 1, "1f10eb10bfc54e336b5f2e2a5c05001817700491d0c79a7931637a9158c41dc0") -- Depot 1284192
+setManifestid(1284192, "6969368711737335786", 6016936746)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3142540) -- The Planet Crafter - Planet Humble
+addappid(4078590) -- The Planet Crafter - Toxicity
