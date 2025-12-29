a9b17037f487eb5c085a7be4f337f99749640866
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2025-12-29 17:41:07 +0800

    Add files via upload

---
+++ 3241660.lua
@@ -0,0 +1,12 @@
+-- 3241660's Lua and Manifest Created by Morrenus
+-- R.E.P.O.
+-- Created: November 30, 2025 at 09:20:26 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3241660) -- R.E.P.O.
+-- MAIN APP DEPOTS
+addappid(3241661, 1, "0a0fcffb2de6fb0fa6caff076a5a8cb960903d17320968d5c042fd5a4f9ad2ad") -- Depot 3241661
+setManifestid(3241661, "180069324351455863", 1736981393)
