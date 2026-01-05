669d535223932e122374ed081167a51807d6559e
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-01-05 17:38:33 +0800

    Add files via upload

---
+++ 2709570.lua
@@ -0,0 +1,20 @@
+-- 2709570's Lua and Manifest Created by Morrenus
+-- Supermarket Together
+-- Created: December 22, 2025 at 08:31:10 EST
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 3
+-- Total DLCs: 1
+-- Shared Depots: 2
+
+-- MAIN APPLICATION
+addappid(2709570) -- Supermarket Together
+-- MAIN APP DEPOTS
+addappid(2709571, 1, "7fa16318d187d201cd5762fec6ac1022fb99b54b7ecb11f228286d5ee2a9790e") -- Depot 2709571
+setManifestid(2709571, "5123962829614523976", 9766938530)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3146530) -- Supermarket Together - The Cool Pack
