e1f27a258db75eb5f9b239b168f419df9c5d2d62
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-01-12 22:21:02 +0800

    Add files via upload

---
+++ 976310.lua
@@ -0,0 +1,58 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 976310.lua
+addappid(976310)
+setManifestid(228986,"8782296191957114623")
+addappid(976311,0,"ba51ae236d415e34a5aa7366e15b74cd96587031f70d2acaf46ac5b390b0f4d9")
+setManifestid(976311,"6512752024476768865")
+addappid(976312,0,"37130205b90f533304b81a7d155c3f74576759eaab5e9f7d4e92478404205f60")
+setManifestid(976312,"2372612785967955744")
+addappid(976313,0,"b8e08c7b5961c5daa928f561f46d5c8438ca9c016ed0c5e9bbe5827d0b7dfdb7")
+setManifestid(976313,"4245947103270175452")
+addappid(1316183,0,"ad7b646a99d28411d65e8f72de9a641edd4c778f7bfb98681eb63f2e0e19cfb1")
+setManifestid(1316183,"2020495199252320891")
+addappid(1461160,0,"d8a15e3cfcd6d44dd3cb30fb1477f61b3b5e6f5cdbf54c2148e2aab61d609ba1")
+setManifestid(1461160,"5735050710340606220")
+-- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
+addappid(1102090) -- Shang Tsung Movie Skins
+addappid(1054480) -- Kombat Pack 1
+addappid(1054870) -- Kold War Skarlet
+addappid(1054871) -- Cangaceiro Kano
+addappid(1054872) -- Gold Demon Scorpion
+addappid(1054873) -- Ninja Mime Johnny Cage
+addappid(1449880) -- Kombat Pack 2
+addappid(1449881) -- Rain
+addappid(1449882) -- Mileena
+addappid(1449883) -- Rambo
+addappid(1449890) -- Time Warriors Skin Pack
+addappid(1200550) -- The Joker
+addappid(1200551) -- Time Lord of Apokolips Geras
+addappid(1200552) -- DC Elseworlds Skin Pack
+addappid(1480230) -- Klassic MK Movie Skin Pack
+addappid(1160240) -- Terminator T-800
+addappid(1160241) -- Double Feature Skin Pack
+addappid(1160242) -- Cassie Quinn
+addappid(1160243) -- Masquerade Skin Pack
+addappid(1180720) -- Sindel
+addappid(1180721) -- Gothic Horror Skin Pack
+addappid(1180722) -- Klassic Cassie
+addappid(1253300) -- Spawn
+addappid(1253301) -- Hellspawn Jacqui
+addappid(1253302) -- Matinee Skin Pack
+addappid(1116620) -- Nightwolf
+addappid(1116621) -- Klassic Arcade Fighter Pack
+addappid(1116622) -- War Games Sonya Blade
+addappid(1316180) -- Fujin
+addappid(1316181) -- Sheeva
+addappid(1316182) -- Robocop
+addappid(1316184) -- Eternal Klash Skin Pack
+addappid(1316185) -- Thanks A Million Johnny Cage
+addappid(1415130) -- All Hallows' Eve Skin Pack
+addappid(1375580) -- Summer Heat Skin Pack
+addappid(1051620) -- Shao Kahn
+addappid(1051621) -- Frost
+addappid(1396460) -- Klassic Femme Fatale Skin Pack
+addappid(1273970) -- Mortal Kombat 11: Aftermath + Kombat Pack Bundle
+addappid(1273971) -- Mortal Kombat 11: Aftermath Expansion
+addappid(1095030) -- Shang Tsung
+addappid(1095031) -- Klassic Ninja Skins
+addappid(1471870) -- Ultimate Add-On Bundle
