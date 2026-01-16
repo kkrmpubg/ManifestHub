b0205f3b54d9ba063b100cae2e689f40353c734b
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-01-17 04:50:21 +0800

    Add files via upload

---
+++ 1119730.lua
@@ -0,0 +1,11 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 1119730.lua
+addappid(1119730)
+setManifestid(228989,"3514306556860204959")
+setManifestid(228990,"1829726630299308803")
+setManifestid(229007,"4477590687906973371")
+setManifestid(229033,"2059065101492814639")
+addappid(1119731,0,"5ba0690900411cc3e2378222e0e437425d26e3b81707810123555cf0c5606a77")
+setManifestid(1119731,"4004464256805224807")
+-- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
+addappid(3255980) -- Ranch Simulator: Southwest Ranch & Farm Expansion Pack
