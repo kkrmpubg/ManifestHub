141f884a10d11021c36b7f5ad5d634747218658b
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-01-29 00:46:32 +0800

    Add files via upload

---
+++ 2679460.lua
@@ -0,0 +1,22 @@
+-- Downloaded using DepotBox - https://depotbox.org/
+-- Original file: 2679460.lua
+addappid(2679460)
+setManifestid(228989,"3514306556860204959")
+addappid(2679461,0,"0a945c91ddfaaca28d19da03981f783f8eb23a6dcc1121fb8c0936bb17fe0adb")
+setManifestid(2679461,"1417753434076416786")
+-- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
+addappid(2924480) -- Metaphor: ReFantazio - Archetype EXP Chest Set
+addappid(2924450) -- Metaphor: ReFantazio - Jouin High School Uniform (7), Battle BGM & Battle Jingle Set
+addappid(2999640) -- Metaphor: ReFantazio - Digital Soundtrack
+addappid(2924420) -- Metaphor: ReFantazio - Gekkoukan High School Uniform (7), Battle BGM & Battle Jingle Set
+addappid(2999650) -- Atlus 35th Digital History Book
+addappid(2924490) -- Metaphor: ReFantazio - Adventurer’s Journey Pack
+addappid(2924460) -- Metaphor: ReFantazio - Samurai Garb (7), Battle BGM & Battle Jingle Set
+addappid(2999660) -- Atlus 35th Digital All-Time Best Soundtrack
+addappid(2924430) -- Metaphor: ReFantazio - Seven Sisters High School Uniform (7), Battle BGM & Battle Jingle Set
+addappid(2999630) -- Metaphor: ReFantazio - Digital Artbook
+addappid(2924400) -- Metaphor: ReFantazio - Shujin Academy School Uniform (7), Battle BGM & Battle Jingle Set 
+addappid(2924470) -- Metaphor: ReFantazio - Etrian Odyssey Class Costumes (7) & Battle BGM Set
+addappid(4128470) -- Metaphor: ReFantazio - Essential Digital Strategy Guide 
+addappid(2924440) -- Metaphor: ReFantazio - St. Hermelin High School Uniform (7), Battle BGM & Battle Jingle Set
+addappid(2924410) -- Metaphor: ReFantazio - Yasogami High School Uniform (7), Battle BGM & Battle Jingle Set
