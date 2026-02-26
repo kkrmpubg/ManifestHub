32bfe576280bd61053adad095b7d2fd73cbc5198
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-02-26 16:30:54 +0800

    Add files via upload

--- 3764200.lua
+++ 3764200.lua
@@ -1,1 +1,37 @@
-addappid(3764200)
+-- 3764200's Lua and Manifest Created by Blake
+-- Resident Evil Requiem
+-- Created: February 25, 2026 at 9:52:51 EST
+-- Total Depots: 9
+-- Total DLCs: 4
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3764200) -- Resident Evil Requiem
+
+-- MAIN APP DEPOTS
+addappid(3764201, 1, "") -- Depot 3764201
+--setManifestid(3764201, "5691830764668778509")
+addappid(3764202, 1, "") -- Depot 3764202
+--setManifestid(3764202, "7450784517022361359")
+addappid(3764203, 1, "") -- Depot 3764203
+--setManifestid(3764203, "")
+addappid(3764204, 1, "") -- Depot 3764204
+--setManifestid(3764204, "")
+
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+--setManifestid(228989, "3514306556860204959", 39590283)
+
+-- DLCS WITH DEDICATED DEPOTS
+--DLC 3990800
+addappid(3990800)
+addappid(3990800, 1, "")
+--setManifestid(3990800, "9181787779883827112")
+addappid(3764205, 1, "")
+--setManifestid(3764205, "4436866528626191669")
+--DLC 3990820
+addappid(3990820)
+addappid(3990820, 1, "")
+--setManifestid(3990820, "509708311968316285")
+addappid(3764206, 1, "")
+--setManifestid(3764206, "136565968785594279")
