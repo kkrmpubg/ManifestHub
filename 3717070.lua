bdb7d607ae7694bbdddf432f9c3e1905f843c5ac
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-03-16 21:20:06 +0800

    Add files via upload

---
+++ 3717070.lua
@@ -0,0 +1,13 @@
+addappid(3717070)
+addappid(3717071,0,"1bf7a881a3e1aceec7d3d40622e631e3d7472b7fb4a051051a5e151bb63d7c21")
+setManifestid(3717071,"4856188368828642086")
+addappid(4181540)
+-- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
+addappid(4181570) -- WWE 2K26: Monday Night War Edition Pack
+addappid(4181560) -- WWE 2K26: Attitude Era Edition Pack
+addappid(4181550) -- WWE 2K26: King of Kings Edition Pack
+addappid(4429680) -- WWE 2K26 Monday Night War Edition
+addappid(4125680) -- WWE 2K26: MyRISE Boost
+addappid(4181590) -- WWE 2K26 Ringside Pass Premium Season 1
+addappid(4333560) -- WWE 2K26 Season Pass
+addappid(4357050) -- WWE 2K26 Superstar Mega-Boost
