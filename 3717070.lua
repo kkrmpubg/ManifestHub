90d0141ba2a06d71bd9272f784be88d5a15eabeb
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-03-16 21:27:12 +0800

    Add files via upload

--- 3717070.lua
+++ 3717070.lua
@@ -1,13 +1,17 @@
+----------------------------
 addappid(3717070)
-addappid(3717071,0,"1bf7a881a3e1aceec7d3d40622e631e3d7472b7fb4a051051a5e151bb63d7c21")
-setManifestid(3717071,"4856188368828642086")
-addappid(4181540)
--- Non-Encrypted DLC ID's (Added by LuaTools @https://discord.gg/NGYPY524 )
-addappid(4181570) -- WWE 2K26: Monday Night War Edition Pack
-addappid(4181560) -- WWE 2K26: Attitude Era Edition Pack
-addappid(4181550) -- WWE 2K26: King of Kings Edition Pack
-addappid(4429680) -- WWE 2K26 Monday Night War Edition
-addappid(4125680) -- WWE 2K26: MyRISE Boost
-addappid(4181590) -- WWE 2K26 Ringside Pass Premium Season 1
-addappid(4333560) -- WWE 2K26 Season Pass
-addappid(4357050) -- WWE 2K26 Superstar Mega-Boost
+addappid(3717071, 1, "1bf7a881a3e1aceec7d3d40622e631e3d7472b7fb4a051051a5e151bb63d7c21")
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853")
+addappid(4125680)
+addappid(4181550)
+addappid(4181560)
+addappid(4181570)
+addappid(4181590)
+addappid(4333560)
+addappid(4357050)
+addappid(4429680)
+--[[
+This file belongs to Ahmeds Bot Discord Server
+-----> join discord Server : https://discord.gg/sv6EGxCRnC
+]]
+----------------------------
