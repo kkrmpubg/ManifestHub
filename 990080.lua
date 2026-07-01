73f875273c16ea614ace8d28472ba3a6d53aa3b3
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-01 22:30:48 +0800

    Add files via upload

--- 990080.lua
+++ 990080.lua
@@ -1,46 +1,23 @@
--- Generated Lua Manifest by Solus Bot in Morrenus Games
--- Steam App 990080 Manifest
--- Name: Hogwarts Legacy
--- Generated: 2025-06-11 02:31:41
--- Total Depots: 13
--- Total DLCs: 4
--- Shared Depots: 4
-
--- MAIN APPLICATION
-addappid(990080) -- Hogwarts Legacy
-
--- MAIN APP DEPOTS
-addappid(990081, 1, "99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6") -- Main Game Content (Windows Content)
-setManifestid(990081, "7657511114935696635", 0)
-addappid(990082, 1, "ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e") -- Game Content (Linux Binaries)
-setManifestid(990082, "2131911399630779386", 0)
-addappid(990083, 1, "2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5") -- Game Content (Mac Content)
-setManifestid(990083, "5520381994780129747", 0)
-addappid(990084, 1, "f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0") -- Common Library Files (Content)
-setManifestid(990084, "4298775494378965915", 0)
-addappid(990085, 1, "b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996") -- Main Game Content (Depot 990085)
-setManifestid(990085, "8141128345180509732", 0)
-addappid(990086, 1, "d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667") -- Main Game Content (Depot 990086)
-setManifestid(990086, "8889891247215635520", 0)
-addappid(990087, 1, "42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75") -- Main Game Content (Depot 990087)
-setManifestid(990087, "8956076021982921820", 0)
-addappid(990088, 1, "81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95") -- Main Game Content (Depot 990088)
-setManifestid(990088, "7612209164717793334", 0)
-addappid(990089, 1, "21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b") -- Main Game Content (Depot 990089)
-setManifestid(990089, "5406006200511624287", 0)
-
--- SHARED DEPOTS (from other apps)
-addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
-setManifestid(228988, "6645201662696499616", 0)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
-setManifestid(228989, "550968249685141759", 0)
-addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
-setManifestid(228990, "1829726630299308803", 0)
-addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
-setManifestid(229005, "7992454656023763365", 0)
-
--- DLCS WITHOUT DEDICATED DEPOTS
-addappid(1880830) -- Hogwarts Legacy Onyx Hippogriff Mount
-addappid(1880832) -- Hogwarts Legacy Dark Arts Pack
-addappid(1880833) -- Hogwarts Legacy Kelpie Robe
-addappid(1880837) --  Hogwarts Legacy Dark Arts Garrison Hat
+addappid(990080)
+addappid(1880830)
+addappid(1880832)
+addappid(1880833)
+addappid(1880837)
+addappid(990081,0,"99edd7f3221635d93481770b28ebac240488e1f42cf16f01e5a4e744b0944da6")
+setManifestid(990081,"5198899101792588169")
+addappid(990082,0,"ac55581fbdef1f6687f91654d435e2d4c395d2fa56eb5aba4fa52d708118157e")
+setManifestid(990082,"7167310505439807086")
+addappid(990083,0,"2326f907ccd6ebfc8235495fbb2abd7b12d814a3228cd3b0bf480f7c602c87f5")
+setManifestid(990083,"6256883361539505915")
+addappid(990084,0,"f043f2ca0cfbbaf5fcb6e30d4beb1a20ba5ef293db11dc4e5521bdfa8a7fdfe0")
+setManifestid(990084,"4878463996609114776")
+addappid(990085,0,"b7d07ff314786df30f9d32266a2e88cd026fabaffbd2567dbc1637cd319ef996")
+setManifestid(990085,"109909038347739340")
+addappid(990086,0,"d9814846faaa5f83e45151bec5190062e320c6b9a6842e7722ed51293a29a667")
+setManifestid(990086,"7173744932268868207")
+addappid(990087,0,"42c46e0e1520cefc9f4120f00fca640bc83f48b322e7b19ad9530538d9cbee75")
+setManifestid(990087,"4408608444924365738")
+addappid(990088,0,"81c364d84279c2fafb7960d0aabd75e7e371ad27f275484d99ef5734e4c00d95")
+setManifestid(990088,"4209668305361446309")
+addappid(990089,0,"21886ab063a78ccf9b676f311597b895c73a1410cd9f7e0e3e7b4e99b82f1e6b")
+setManifestid(990089,"7833413172600852139")
