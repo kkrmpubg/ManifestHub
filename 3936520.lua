7de4f9cf1440b3c4113c4ae7ed4ec7c26204e269
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-05 17:33:41 +0800

    Add files via upload

---
+++ 3936520.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3936520 - Tour de France 2026
+-- Generated 2026-07-04 23:28:59 UTC
+-- # Depots (Total/DLC/Shared): 4/0/3
+
+-- Main AppID
+addappid(3936520)
+
+-- Main Depots
+addappid(3936521, 1, "54a7be6f753caa5473c9f098b40a9c1519ca57f4d0985f84d940de9812450675")
+setManifestid(3936521, "2999476546498220553", 33836056368)
+
+-- DLC's (no depot keys required)
+addappid(4502380) -- Tour de France 2026 - Spanish Grand Départ Pack
+addappid(4502390) -- Tour de France 2026 - Upgrade to Deluxe
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- (windows)
