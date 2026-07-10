d4f0d37d5430dce81d8833a112d2e78a495469e5
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-10 10:55:37 +0800

    Add files via upload

---
+++ 2244210.lua
@@ -0,0 +1,31 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2244210 - Echoes of Aincrad
+-- Generated 2026-07-10 02:43:04 UTC
+-- # Depots (Total/DLC/Shared): 5/2/2
+
+-- Main AppID
+addappid(2244210)
+
+-- Main Depots
+addappid(2244211, 1, "105c2f87ad76d3549179dfbeb54c0248a1bde5a72ba14d74714e480d4706f57e")
+setManifestid(2244211, "1527046485629517929", 38561768950)
+
+-- DLC's (no depot keys required)
+addappid(4249240) -- Echoes of Aincrad - Death Game Mode Early Unlock
+addappid(4249250) -- Echoes of Aincrad - Weapon Pack: Proto-Elucidator Series
+addappid(4249260) -- Echoes of Aincrad - Armor Pack: Flutter Boots & Flutter Robes
+addappid(4249270) -- Echoes of Aincrad - Starter Pack
+addappid(4337740) -- Echoes of Aincrad - Expansion Pack
+addappid(4337770) -- Echoes of Aincrad - Ultimate Upgrade Pack
+
+-- DLC's (with depot keys)
+-- Echoes of Aincrad Unanswered//butterfly Bonus Contents App (AppID: 3443750)
+addappid(3443750)
+addappid(3443750, 1, "85140ce089e2a2c387357acfbc4408d975400ab69786255011f7a1d0bc406d2d")
+setManifestid(3443750, "310538696736326014", 4642644659)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3996330
