16d0f232a8b738e85717e39955ea0d6b50f6a70c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-11 20:59:11 +0800

    Add files via upload

--- 1001270.lua
+++ 1001270.lua
@@ -1,4 +1,14 @@
-addappid(1001270)
-addappid(3821410)
-addappid(1001271,0,"840a384558d951071f078063810429d5c2943f4389e02a2b992ad4e6cb001088")
-setManifestid(1001271,"7356222977622691126")
+-- Generated with Luie @ https://lua.tools/
+-- 1001270 - Kebab Chefs! - Restaurant Simulator
+-- Generated 2026-07-11 12:58:29 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1001270, 1, "b5ecb422903569d873f7818e7c4d3f91e567c78f85d806607fc7eaf62d10afad")
+
+-- Main Depots
+addappid(1001271, 1, "840a384558d951071f078063810429d5c2943f4389e02a2b992ad4e6cb001088")
+setManifestid(1001271, "589298356051494204", 7393391059)
+
+-- DLC's (no depot keys required)
+addappid(3821410) -- Kebab Chefs - Bakery Shop
