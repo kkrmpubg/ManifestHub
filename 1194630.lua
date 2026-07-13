5faddeef59f8249f55c42ab1208b72b6091409b7
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-14 03:09:59 +0800

    Add files via upload

---
+++ 1194630.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1194630 - The Dark Pictures Anthology: Little Hope
+-- Generated 2026-07-13 19:09:03 UTC
+-- # Depots (Total/DLC/Shared): 6/0/2
+
+-- Main AppID
+addappid(1194630, 1, "b78d5cc910ae314b2296d3f2425b858dc7bbe7034bfe035861adfd3dc37b3036")
+
+-- Main Depots
+addappid(1194633, 1, "c2a60fdea7b1519ad4f79cde6079b8cd3894fa5884ea554071369734d3977594")
+setManifestid(1194633, "2401317617118803807", 46123882745)
+addappid(1194634, 1, "2d0d066d3183f8a09ff116446a893895daeda82fbb6911588ed7ef21f4814f60")
+setManifestid(1194634, "7353019674043464160", 16)
+
+-- DLC's (no depot keys required)
+addappid(1259810) -- The Dark Pictures Anthology: Little Hope - Curator's Cut
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 1194631, 1194639
