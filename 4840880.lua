1d6629be416c7838e781aaa7c6c996e5cb0035e8
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-13 19:43:45 +0800

    Add files via upload

---
+++ 4840880.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4840880 - ESKINITA
+-- Generated 2026-07-13 11:41:47 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4840880)
+
+-- Main Depots
+addappid(4840881, 1, "f3832461c34bd1bbd7bb1f18073ae6042e4cb0390f11307d7c5a86201b5959c3") -- English (windows)
+setManifestid(4840881, "7007437636461504551", 523926666)
