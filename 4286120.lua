beb9174dbf5bd141e46c410839d4a73f2e993f7f
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-22 03:28:36 +0800

    Add files via upload

---
+++ 4286120.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4286120 - Photomaly
+-- Generated 2026-07-21 18:38:57 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4286120)
+
+-- Main Depots
+addappid(4286121, 1, "8ba184028e7c93e71bf1be655408324148b30e3f1201b728dc7dbac93a581fe1") -- (windows)
+setManifestid(4286121, "1740150175097365140", 1892243747)
