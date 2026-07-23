4b163b025d5bf99f94555d42a8a0e5d0916e758c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-24 04:26:45 +0800

    Add files via upload

---
+++ 2806050.lua
@@ -0,0 +1,21 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2806050 - Halo: Campaign Evolved
+-- Generated 2026-07-23 19:48:20 UTC
+-- # Depots (Total/DLC/Shared): 4/1/2
+
+-- Main AppID
+addappid(2806050)
+
+-- Main Depots
+addappid(2806051, 1, "400618d2330230a74b26331220f2aed43a92bf65eb23147779810f48f893c6e5")
+setManifestid(2806051, "8153709523381701809", 76619192154)
+
+-- DLC's (with depot keys)
+-- DLC 4192200 (AppID: 4192200)
+addappid(4192200)
+addappid(4192200, 1, "ba2cc915e2f0112e4af89f7147a980880639c08aed6b47703282f5175355a021")
+setManifestid(4192200, "7241924470348670959", 2889807339)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
