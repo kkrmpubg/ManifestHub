91f5c58fce89e3c1b3e91d16380eec04389c46b4
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-23 09:20:10 +0800

    Add files via upload

---
+++ 4339880.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4339880 - Game Compressor
+-- Generated 2026-07-23 00:57:34 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4339880)
+
+-- Main Depots
+addappid(4339881, 1, "068f5329555cc9874fce0e22d2d0fc71ce32dcef1c4e45e83241a4856348e9bb") -- (windows, 64-bit)
+setManifestid(4339881, "6639089360093276134", 185287445)
