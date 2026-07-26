d11a7319edb4541abb1ae6d547e3f8dd89f89b70
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-26 17:12:36 +0800

    Add files via upload

---
+++ 3722330.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3722330 - Shift At Midnight
+-- Generated 2026-07-26 08:17:10 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(3722330)
+
+-- Main Depots
+addappid(3722331, 1, "241b88aeb27a250cbfff336e4e5ec6f28a60d136f4431f8bdcbf84a0506e5ed4")
+setManifestid(3722331, "6950982369220271334", 870853520)
