87eb1205fbb5016d74648e27d423249673d7a429
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-07-31 23:40:15 +0800

    Add files via upload

---
+++ 1913120.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1913120 - Tears of Metal
+-- Generated 2026-07-31 15:39:30 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1913120, 1, "c789b529828e652dbd3f3e408af75a733fef864e5728655a076aac0d14145f6e")
+
+-- Main Depots
+addappid(1913121, 1, "ef09faa539a0032c86e138039553cfb95ed24ac0a3af18e948854bf7d108cbea")
+setManifestid(1913121, "1507899492393951603", 3785946182)
