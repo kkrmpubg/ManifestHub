147d356b6e084a6cde824beb613f2803d6c543ac
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-07 20:31:44 +0800

    Add files via upload

---
+++ 4397060.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4397060 - Sitting Ducks
+-- Generated 2026-08-07 12:30:50 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4397060, 1, "c3cbbb7ac5161993432c777333fee34c3b6e2dbf89aadb2025684c7fb44b9d0d")
+
+-- Main Depots
+addappid(4397061, 1, "29cbf9a54c5a3b1ebb10307cb864ec39d6a07ebf12092cbb1cfa845a1d0053d6")
+setManifestid(4397061, "3178045153778614878", 1763705987)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
