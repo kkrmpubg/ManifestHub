4fe79dd4aa828ca23bb5b2db3a33c2cf4d939115
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-13 21:25:35 +0800

    Add files via upload

---
+++ 4333200.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4333200 - Lost & Found: A This Bed We Made Story
+-- Generated 2026-08-13 13:19:20 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(4333200, 1, "62bfbbb7c598313c200e5087f4203b012841f1dc700428254d8e56e5f8e3e92c")
+
+-- Main Depots
+addappid(4333201, 1, "c6ef4bf622688db0394ee043858049682bd14d69e02a959f87e7f83b3917bd27")
+setManifestid(4333201, "7722886653119526981", 3431158031)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
