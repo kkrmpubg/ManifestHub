60780a6c4f109164215296d9574fd5e8810d9cb9
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-13 19:28:43 +0800

    Add files via upload

---
+++ 1868140.lua
@@ -0,0 +1,45 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1868140 - DAVE THE DIVER
+-- Generated 2026-08-13 11:27:44 UTC
+-- # Depots (Total/DLC/Shared): 14/12/0
+
+-- Main AppID
+addappid(1868140, 1, "c283d28f5a647e86857c71bbcd79f63db35febefefe20b121df6dc6a8721e673")
+
+-- Main Depots
+addappid(1868141, 1, "dec1482ee88e2a62d7fddedf0e75c6abdef55d2884440de6508bef83044198d8") -- (windows)
+setManifestid(1868141, "1490153896524122226", 5917371884)
+addappid(1868142, 1, "f55cec1c353af17ba294e0bc3c1af6766a91b76e97d59d44425873918058d6c5") -- (macos)
+setManifestid(1868142, "7107957488412882591", 5941020160)
+
+-- DLC's (with depot keys)
+-- DAVE THE DIVER Digital Extra (AppID: 2492320)
+addappid(2492320)
+addappid(2492320, 1, "80467121a86d6b38971889737a91b309bb3aa329fee8e93ac63f2748f43fa063")
+setManifestid(2492320, "3605329027721964993", 233287510)
+-- DAVE THE DIVER - DREDGE Content Pack (AppID: 2677020)
+addappid(2677020)
+addappid(2677020, 1, "0f12757b1a8cc78265828ba98e12b673ff97e942bbfd3ae07eca6d10f6b8dfcf") -- (windows)
+setManifestid(2677020, "1640036140298738282", 27270157)
+addappid(2677021, 1, "794d4476c6913ad8568af8bf1303f216b1f16b3d6041f62834536fe460d661dd") -- (macos)
+setManifestid(2677021, "4629321012438313755", 27277403)
+-- DAVE THE DIVER - Godzilla Content Pack (AppID: 2841140)
+addappid(2841140)
+addappid(2841140, 1, "a5e4472216eb53c5abc48c617b42bc740a1fac4adf25659c183015a27f8849fb") -- (windows)
+setManifestid(2841140, "4277352553098425670", 71790521)
+addappid(2841141, 1, "a6863e71aca974796ecc6423cb6d8b9820fd11e9df7f06bcbaa7a1926227f77d") -- (macos)
+setManifestid(2841141, "415104347584392154", 71937165)
+-- DAVE THE DIVER - Ichiban's Holiday Content Pack (AppID: 3543180)
+addappid(3543180)
+addappid(3543180, 1, "5dbf7b425cf6ac47d95a2abe30926e2c845f6b9fbf05903c27837624a56b6b66") -- (windows)
+setManifestid(3543180, "7053194086829219553", 210383241)
+addappid(3543181, 1, "fda6ac6e3da3645d6c445ad4c68bffd11640df814da698f3fb85db7ada2a388b") -- (macos)
+setManifestid(3543181, "6888995605330603482", 206894045)
+-- DAVE THE DIVER - In the Jungle Content Pack (AppID: 4394810)
+addappid(4394810)
+addappid(4394810, 1, "ce700da5762e56b089453223d437b355b58433869cb2fab565b0c6c0837d87e2") -- (windows)
+setManifestid(4394810, "6080006835337499181", 0)
+addappid(4394811, 1, "07d0577ef5b0833c904eb284e46eeea83cfc030963793fdf43aac48c95cdbf1f") -- (macos)
+setManifestid(4394811, "2071559694774481152", 794745894)
+
+-- Missing Depots (We don't have the keys yet lol): 2463870, 4158580, 4158581
