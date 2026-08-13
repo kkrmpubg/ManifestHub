7d56ade6e46924b350fdc4c0fcc5c6c72fa914e4
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-13 23:37:51 +0800

    Add files via upload

---
+++ 3280350.lua
@@ -0,0 +1,47 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3280350 - DEATH STRANDING 2: ON THE BEACH
+-- Generated 2026-08-13 14:59:31 UTC
+-- # Depots (Total/DLC/Shared): 16/1/1
+
+-- Main AppID
+addappid(3280350, 1, "914f5e9b7272a17fea869d253548261383245b5da4406b89fe63bea6fe6634e5")
+
+-- Main Depots
+addappid(3280351, 1, "adedba4e249ea03aa263769ddb3bccd36a54816bbe471c73c51c5b1569187d00") -- (windows, 64-bit)
+setManifestid(3280351, "1384563182965223281", 118810744199)
+addappid(3280352, 1, "4a43534f83813efc865429cc3edb56f45db455c86e8373827711e5c447f0d7e3") -- (windows, 64-bit)
+setManifestid(3280352, "7935445175049470697", 928109425)
+addappid(3280354, 1, "7010c7c1c501ba95ec42f5545dcfc4ee462ca97e854e4c0c3eccb61cbaf26c84") -- (windows, 64-bit)
+setManifestid(3280354, "6192873018491771353", 1010413673)
+addappid(3280355, 1, "50cfe1f0f242308653ad05a8b8fe9584fa351541b3d791dc5aad88551f1c7d7e") -- Japanese (windows, 64-bit)
+setManifestid(3280355, "8778380903344259947", 1076004379)
+addappid(3280356, 1, "07766d818c98a30b9be759700331449b058d782e839b4aac64b95657b6c82874") -- French (windows, 64-bit)
+setManifestid(3280356, "1048323885302250340", 1021239939)
+addappid(3280357, 1, "2a099de8b315d9bb7ad6ccd9589c278e59cbee17b5d6661c886a0d9c9ec84f86") -- Spanish (windows, 64-bit)
+setManifestid(3280357, "6907394593365258098", 1028249495)
+addappid(3280358, 1, "9c26057431edb93a11cad7a246c2b765391eaf18e38d1bd8a9c2a7433fc69aaf") -- German (windows, 64-bit)
+setManifestid(3280358, "7310126732521444880", 1020901770)
+addappid(3280359, 1, "1da8c06ccf9373407642f3320f2150fc3fc35020faf8967c6a506e3d882ab34a") -- Italian (windows, 64-bit)
+setManifestid(3280359, "3501722085141087279", 1021246055)
+addappid(3669121, 1, "b25594517feb73aa10cd13ffc9507e027aac8dd2fe4c451547f6b4401f74d318") -- Portuguese (windows, 64-bit)
+setManifestid(3669121, "7911449249511130970", 1002978229)
+addappid(3669122, 1, "640832b483b5a8f5dac50d55e17ef9a31d9b5f58f29b6ea5c85a41a0dfb0342b") -- Russian (windows, 64-bit)
+setManifestid(3669122, "5648819044024192892", 1039757434)
+addappid(3669123, 1, "a2aba5b5b71722e792d695f883c5d6b62ed57f286bc0b9d3b4f84578f1335076") -- Polish (windows, 64-bit)
+setManifestid(3669123, "3798818004235048972", 1017863674)
+addappid(3669124, 1, "647404e49ca916a3796b7839067145587e75bcb75957e706a0979ae21dc25d0f") -- Latam (windows, 64-bit)
+setManifestid(3669124, "5935004088472077409", 1019284202)
+addappid(3669125, 1, "33b649ba960e4412464f28b70beb09f3567216daaa0bbd08879019f78d945b1f") -- Brazilian (windows, 64-bit)
+setManifestid(3669125, "7925816387879957204", 955480106)
+addappid(3669126, 1, "b95d6ff8f985523cf9b2826e9eac6d66c0a794de6d064972b193fffc7ca538b7") -- Schinese (windows, 64-bit)
+setManifestid(3669126, "3076020091720019374", 952288805)
+
+-- DLC's (no depot keys required)
+addappid(3669120) -- DEATH STRANDING 2: ON THE BEACH - Digital Deluxe Content
+addappid(3669170) -- DEATH STRANDING 2: ON THE BEACH - Upgrade to Digital Deluxe Edition
+addappid(4119250) -- DEATH STRANDING 2: ON THE BEACH - Pre Purchase Content
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3280353
