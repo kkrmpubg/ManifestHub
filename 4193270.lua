01e1763dedf2aecaf4a9db20a46fd2e2b43c646c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-14 23:43:01 +0800

    Add files via upload

---
+++ 4193270.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4193270 - 日本事故物件監視協会2 -Japan Stigmatized Property-
+-- Generated 2026-08-14 15:41:33 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4193270)
+
+-- Main Depots
+addappid(4193271, 1, "e244dc403ccdd385bb6d7a3cd66dc333718dc1866a91ada1032294f52d485029") -- (windows, 64-bit)
+setManifestid(4193271, "4429596019300047933", 2331782300)
+
+-- Missing Depots (We don't have the keys yet lol): 4193273
