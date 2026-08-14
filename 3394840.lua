a74b6467cf8123f21f65b73ace7244d001b203eb
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-15 00:20:31 +0800

    Add files via upload

---
+++ 3394840.lua
@@ -0,0 +1,15 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3394840 - Clawed
+-- Generated 2026-08-14 16:05:47 UTC
+-- # Depots (Total/DLC/Shared): 3/0/2
+
+-- Main AppID
+addappid(3394840)
+
+-- Main Depots
+addappid(3394841, 1, "34e22bdc0f2bf98f9a7158ae217d5327977e69ee3e731fda1b4bc2639e4f2674")
+setManifestid(3394841, "79376009569897737", 17555574906)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
