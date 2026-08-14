bec844a0fdb5d9ec31905c563a62a441657787d9
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-14 23:43:57 +0800

    Add files via upload

---
+++ 4611990.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4611990 - Japan Stigmatized Property3 -日本事故物件監視協会-
+-- Generated 2026-08-14 15:43:22 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4611990)
+
+-- Main Depots
+addappid(4611991, 1, "4ed9df1199a53ccc356b697ab4f76168f7b42c633e2a61778854ef4394ab53e6") -- (windows, 64-bit)
+setManifestid(4611991, "1052453175726519794", 2495719825)
+addappid(4611992, 1, "db3934f6fba8242eb940c75df4006c8f74ac9d8d795447f3a718afac2ef3349d") -- (macos, 64-bit)
+setManifestid(4611992, "8880059424764507293", 4084808083)
