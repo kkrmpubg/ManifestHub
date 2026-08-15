56ee3dbda8c3f2dbb3113375c497cc930757002b
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-16 00:59:54 +0800

    Add files via upload

---
+++ 3818020.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3818020 - Evil School
+-- Generated 2026-08-15 14:20:37 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(3818020)
+
+-- Main Depots
+addappid(3818021, 1, "3c3bea1d791631b31077c7668fc448570f9862cc9659dbf5cd10e829859b853f") -- (windows)
+setManifestid(3818021, "7897883800208582230", 25274657340)
+
+-- Missing Depots (We don't have the keys yet lol): 3818022
