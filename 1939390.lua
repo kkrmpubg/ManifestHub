a6fd47a4dd3474ab6832b554e0db96982981bd0c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-16 17:32:29 +0800

    Add files via upload

---
+++ 1939390.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 1939390 - Dino Party
+-- Generated 2026-08-16 09:31:44 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(1939390)
+
+-- Main Depots
+addappid(1939391, 1, "996bcb1d517cc5235fa07180d919281b07bf2b7e7ab8652563b756436231fc22") -- (windows,linux)
+setManifestid(1939391, "3716651980729924007", 1909738925)
