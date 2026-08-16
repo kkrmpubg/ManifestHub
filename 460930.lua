6911fc1612227ec8cc82f76f2b6d14ea41f9327c
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-16 23:29:37 +0800

    Add files via upload

---
+++ 460930.lua
@@ -0,0 +1,117 @@
+-- Generated with Luie @ https://lua.tools/
+-- 460930 - Tom Clancy's Ghost Recon® Wildlands
+-- Version-locked to Build 5948128 (No title) — released 2020-12-14 09:06:37 UTC
+-- Generated 2026-08-07 16:14:28 UTC
+-- # Depots (Total/DLC/Shared): 30/2/4
+
+-- Main AppID
+addappid(460930, 1, "c25d8c77b47191a3d41e556d99fe3adae1457c74f6f6bed925f6e57f3541cfda")
+
+-- Main Depots
+addappid(460932, 1, "bde2110e49d13530ba0d862bf38a2f1da77ebca381bb46123dd94992b4566bbf") -- (windows, 64-bit)
+setManifestid(460932, "8824930428821563913")
+addappid(460933, 1, "90d2ad5b74312c5928a1ac0dcf3b30f071f44dc683c546aeb1ec515464aa442b") -- (windows, 64-bit)
+addappid(460934, 1, "13ce87f397410fb2e07a8eec31bcd08d89ab99d3f69dc55bf992f2438fc85012") -- (windows, 64-bit)
+setManifestid(460934, "4444935984325081985")
+addappid(460935, 1, "cb668cc06add491f81676b5b5e74ee1197422be29ce3613f2c74ecd0d1c699b8") -- (windows, 64-bit)
+setManifestid(460935, "9199072077909944392")
+addappid(460936, 1, "b544bb4108b7ae8c010295e4fac8a1479bb9e89da5df5af021d42b9c8a9b1d1d") -- (windows, 64-bit)
+setManifestid(460936, "8051295774455126082")
+addappid(460937, 1, "d36441c770d9d5c8cf8d772e3b81ef84556fda68e52af96a9b0c5b16b1d04aa0") -- (windows, 64-bit)
+setManifestid(460937, "1905479328869316905")
+addappid(460938, 1, "27d9c76eade84acdd844666a43364165287be4f612e8427e02c727d08bdb6576") -- (windows, 64-bit)
+setManifestid(460938, "5496547418910000312")
+addappid(460939, 1, "5efd1f1fe70217560d4cff479ea20def14fec7279f1850a12435e7e5ec95e39f") -- (windows, 64-bit)
+setManifestid(460939, "2012683680661394186")
+addappid(527050, 1, "f10cb78d153689ea0177527be3d7b93d805333df0d03eb32a724eda5fb62f7fb") -- French (windows, 64-bit)
+setManifestid(527050, "6133416095183290669")
+addappid(527051, 1, "7de4c108fe2624e7eb85d16984171d5646021b28efefdf0b48a9052fe5545d9b") -- Italian (windows, 64-bit)
+setManifestid(527051, "8704848836597241185")
+addappid(527052, 1, "0c465a66d1535d9a0ec9c0593150b6f097cf83cabee96700261ec0bc5d5a012e") -- German (windows, 64-bit)
+setManifestid(527052, "7953425400952113538")
+addappid(527053, 1, "1d5327ab8926812c42541b648727b19f5c5e4d6fe0b70edb05fed99d398e15fc") -- Spanish (windows, 64-bit)
+setManifestid(527053, "3188754202211716325")
+addappid(527054, 1, "4c99be4a2d810244c95983f997c9742ed450d274fc3d1753930af59f41c22b9f") -- Brazilian (windows, 64-bit)
+setManifestid(527054, "2125566137229626735")
+addappid(527055, 1, "e0e96bf59ed92e7db65e7c77059b9f141935835f52782d0351c27610a0a60f4d") -- Russian (windows, 64-bit)
+setManifestid(527055, "7137644527826082354")
+addappid(527056, 1, "6048699beac55e725b81369d008b8ab8252bc97c45e5eb0b608ef93c7d6f338d") -- Japanese (windows, 64-bit)
+setManifestid(527056, "7414914160411395915")
+addappid(527057, 1, "546beca62ef655656b5e3e7d7ef648cc78b3ece40c9b184ecd9802420b3e0acc") -- Dutch (windows, 64-bit)
+setManifestid(527057, "6207633329687923501")
+addappid(527058, 1, "7cc5e490129d7e39967b2f8968c2ec31525f6fb501401f6b16a4e1ae820c627f") -- Polish (windows, 64-bit)
+setManifestid(527058, "2115994499122544238")
+addappid(527059, 1, "bff5883e41209a1c1157f58739a8f4da75fec461f22b2a2151a60d575660ca57") -- Czech (windows, 64-bit)
+setManifestid(527059, "5196520543398736486")
+addappid(527060, 1, "e231cae9d9d2e2c8211dca82c0586807aaaa57939e81870e366dbf40c9c66f8b") -- Koreana (windows, 64-bit)
+setManifestid(527060, "6909220124751877463")
+addappid(527061, 1, "532e7576df7da83b13254a9447e52174d79e2e93e3e32a67c9c77057ba4867ca") -- Schinese (windows, 64-bit)
+setManifestid(527061, "5809069132872559849")
+addappid(527062, 1, "c285d9099dea161bfabdea70627aa3d08883ef105bef6559a05c88c699f5fce1") -- Tchinese (windows, 64-bit)
+setManifestid(527062, "6854033068166781157")
+addappid(527063, 1, "de7a5d543800eafbf2935b8ddddd530e74ded8abdcb238dcb4b876eb98ea6532") -- English (windows, 64-bit)
+setManifestid(527063, "222418595712558555")
+addappid(527064, 1, "020c6de13a43f818050fdf62802aed3eafa69e14002e4128d4f4598784125700") -- Arabic (windows, 64-bit)
+setManifestid(527064, "8291120964054423790")
+addappid(527065, 1, "4a4a61de29327f30e4246ffd40b77b6968e40965d4e2dd687b1942016b672276") -- Latam (windows, 64-bit)
+
+-- DLC's (no depot keys required)
+addappid(573830) -- Tom Clancy's Ghost Recon Wildlands - Season Pass
+addappid(594560) -- Tom Clancy's Ghost Recon Wildlands - WW Uplay Activation
+addappid(594561) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Uplay Activation
+addappid(601290) -- Tom Clancy's Ghost Recon Wildlands - WW Pre-order Standard Uplay Activation
+addappid(601291) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Standard Uplay Activation
+addappid(601292) -- Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Standard Uplay Activation
+addappid(601293) -- Tom Clancy's Ghost Recon Wildlands - WW Pre-order Deluxe Uplay Activation
+addappid(601294) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Deluxe Uplay Activation
+addappid(601295) -- Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Deluxe Uplay Activation
+addappid(601296) -- Tom Clancy's Ghost Recon Wildlands - WW Pre-order Gold Uplay Activation
+addappid(601297) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Pre-order Gold Uplay Activation
+addappid(601298) -- Tom Clancy's Ghost Recon Wildlands - ASIA Pre-order Gold Uplay Activation
+addappid(601300) -- Tom Clancy's Ghost Recon Wildlands - WW Deluxe Uplay Activation
+addappid(601301) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Deluxe Uplay Activation
+addappid(601302) -- Tom Clancy's Ghost Recon Wildlands - WW Gold Uplay Activation
+addappid(601303) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Gold Uplay Activation
+addappid(601310) -- Tom Clancy's Ghost Recon Wildlands - Season Pass Uplay Activation
+addappid(623780) -- DLC 623780
+addappid(623781) -- DLC 623781
+addappid(729730) -- DLC 729730
+addappid(733960) -- DLC 733960
+addappid(750230) -- Tom Clancy's Ghost Recon Wildlands - Ghost War Pass
+addappid(751960) -- DLC 751960
+addappid(832700) -- Tom Clancy's Ghost Recon Wildlands - Y2
+addappid(832701) -- DLC 832701
+addappid(931190) -- DLC 931190
+addappid(931191) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Gold Year 2 Uplay Activation
+addappid(931192) -- DLC 931192
+addappid(931193) -- Tom Clancy's Ghost Recon Wildlands - RU/CN Ultimate Year 2 Uplay Activation
+addappid(4784340) -- Tom Clancy's Ghost Recon Wildlands - Definitive Edition Ubisoft Activation -WW
+addappid(4784350) -- Tom Clancy's Ghost Recon Wildlands - Definitive Edition Ubisoft Activation - RU/CN
+addappid(4784380) -- Tom Clancy's Ghost Recon Wildlands: Cold Eye Pack
+addappid(4784390) -- Tom Clancy's Ghost Recon Wildlands: Cold Eye Pack Ubisoft Activation
+addappid(4784400) -- Tom Clancy's Ghost Recon Wildlands: Nightfall Protocol Pack
+addappid(4784410) -- Tom Clancy's Ghost Recon Wildlands: Nightfall Protocol Pack Ubisoft Activation
+addappid(4784420) -- Tom Clancy's Ghost Recon Wildlands - Predator Premium Pack
+addappid(4784430) -- Tom Clancy's Ghost Recon Wildlands - Predator Premium Pack Ubisoft Activation
+addappid(4784440) -- Tom Clancy's Ghost Recon Wildlands: Definitive Edition Upgrade
+addappid(4784450) -- Tom Clancy's Ghost Recon Wildlands: Definitive Edition Upgrade Ubisoft Activation
+
+-- DLC's (with depot keys)
+-- Tom Clancy's Ghost Recon Wildlands - Narco Road (AppID: 573831)
+addappid(573831)
+addappid(573831, 1, "b98f4b2691d92850b6dbd670b4259b24b37f748b2019344233308f1ac849ba0d") -- (windows, 64-bit)
+setManifestid(573831, "504287665923200055")
+-- Tom Clancy's Ghost Recon Wildlands - Fallen Ghosts (AppID: 573832)
+addappid(573832)
+addappid(573832, 1, "8b0e38a0d31e60759feb1870c81f6a1e54b8373e109942f232ac04b886d6edd3") -- (windows, 64-bit)
+setManifestid(573832, "1388355723944228642")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228984, 1, "df7df98d3134f5e0009d5ebcaaf3bbb91ea2f7cbad81a37a9d16bc1835f57068") -- (windows)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- (windows)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
+addappid(1716751, 1, "84780b728a23b1dabbe8b064807ccd3dbd40c67139ed569101104a418c581675")
+
+-- Token-locked DLCs (couldn't read depots): 733970
+
+-- Too new to lock (no manifest at this build): 460933, 527065
