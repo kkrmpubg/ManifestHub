e44de569c181a460c338ba6cb53635aa793f1ce9
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-19 17:51:17 +0800

    Add files via upload

---
+++ 3314340.lua
@@ -0,0 +1,22 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3314340 - Burgie's cozy kitchen
+-- Generated 2026-08-19 09:50:36 UTC
+-- # Depots (Total/DLC/Shared): 2/1/0
+
+-- Main AppID
+addappid(3314340)
+
+-- Main Depots
+addappid(3314342, 1, "4a553b925ea338f09b056513e1f0feeb7aea85a2ae975aa82cbc38b5019bf985") -- (windows)
+setManifestid(3314342, "7843333170002492666", 546791841)
+
+-- DLC's (no depot keys required)
+addappid(4127290) -- Burgie's cozy kitchen - Halloween
+
+-- DLC's (with depot keys)
+-- Burgie's cozy kitchen - Demo (AppID: 4738670)
+addappid(4738670)
+addappid(4738671, 1, "7aae0cc1816a6e1f8d2dd856c5e91e2d5e49c5c465664a28fdcf4fec7bda3d87")
+setManifestid(4738671, "5456471937164010427", 461987500)
+
+-- Token-locked DLCs (couldn't read depots): 4969500
