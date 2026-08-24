9a15dfa8335ce22d1d564b8549538770292d6136
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-24 18:54:26 +0800

    Add files via upload

---
+++ 4874400.lua
@@ -0,0 +1,11 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4874400 - framepacer
+-- Generated 2026-08-24 10:23:05 UTC
+-- # Depots (Total/DLC/Shared): 1/0/0
+
+-- Main AppID
+addappid(4874400)
+
+-- Main Depots
+addappid(4874401, 1, "7b9247573cd6c0017088e6ecca35f305b5185fbbe4d66e1622332e5f01905ed5")
+setManifestid(4874401, "2283517939810112765", 2803306)
