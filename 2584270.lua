4754ba2f92763b401defbd17e932b597984a298e
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-27 02:58:41 +0800

    Add files via upload

---
+++ 2584270.lua
@@ -0,0 +1,20 @@
+-- Generated with Luie @ https://lua.tools/
+-- 2584270 - Mortal Shell II
+-- Generated 2026-08-26 18:37:18 UTC
+-- # Depots (Total/DLC/Shared): 2/0/1
+
+-- Main AppID
+addappid(2584270, 1, "2b9f4a2124d5757f3d4f46c81c6d4082f3b1d1ac9b3233acf41ddc6b5c15810a")
+
+-- Main Depots
+addappid(2584271, 1, "bb59e6dc30df8728a4bfcc3129ca9b8284ed3808d42a9a272e65d4904b4bc445")
+setManifestid(2584271, "2923909788887825334", 73089555491)
+
+-- DLC's (no depot keys required)
+addappid(4711720) -- Mortal Shell II: Devout Edition Upgrade
+addtoken(4711720, "14235145898889595086")
+addappid(4711730) -- Mortal Shell II - Pre-order Bonus
+addtoken(4711730, "2594855405444440804")
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
