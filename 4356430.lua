72ab980d1abe4b2d57b144bc696949a33bacc823
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-27 19:07:59 +0800

    Add files via upload

---
+++ 4356430.lua
@@ -0,0 +1,29 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4356430 - NBA 2K27
+-- Generated 2026-08-27 08:15:11 UTC
+-- # Depots (Total/DLC/Shared): 5/3/1
+
+-- Main AppID
+addappid(4356430)
+addtoken(4356430, "13716260120817756824")
+
+-- Main Depots
+addappid(4356431, 1, "a1ccdda87579dfa269b9015cc7ba18b7eb8339848b5eb408bfea8bdfb31eb8a4")
+setManifestid(4356431, "4426313921998626225", 80455180238)
+
+-- DLC's (no depot keys required)
+addappid(4357560) -- NBA 2K27 Pro Pass: Season 1
+addappid(4357570) -- NBA 2K27 Hall of Fame Pass: Season 1
+addappid(4357580) -- NBA 2K27 MyCAREER Bonus Offer: Season 1
+addappid(4357590) -- NBA 2K27 MyTEAM Bonus Offer: Season 1
+
+-- DLC's (with depot keys)
+-- DLC 4687050 (AppID: 4687050)
+addappid(4687050)
+addappid(4687050, 1, "c96a0954f3836680afb31e3b837546bb92606ccaebcc3e1bf8cd3761b8221c25")
+setManifestid(4687050, "4767089068737929486", 29646686473)
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 4841760, 4841761
