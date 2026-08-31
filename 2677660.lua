b2e9771017c118540732e550a1069ee004ccc43e
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-09-01 04:49:09 +0800

    Add files via upload

--- 2677660.lua
+++ 2677660.lua
@@ -1,50 +1,18 @@
--- Generated with Luie @ https://lua.tools/
--- 2677660 - Indiana Jones and the Great Circle
--- Generated 2026-08-31 20:03:28 UTC
--- # Depots (Total/DLC/Shared): 14/1/1
+addappid(2677660)
+addappid(228989)
+addappid(2677661,0,"0abf0860b7a1d53e5ae7b3ed8d7e97e668fa5619be6a6ad74c140c53871868bd")
+addappid(2677662,0,"b4089fc44110008f2b07bc97f30e298c1ae457fa69964cdb239f480259baec84")
+addappid(2830501,0,"43cf88c0f48b755f8cb8d54bf6f3a04a2ed5551120d29b81c8ae098f26edbb0e")
+addappid(2830502,0,"c9fbb0008c00893bad5a3815793a4439d6a7d3ebcdc9bc6fe187a30f2ab326c7")
+addappid(2830503,0,"02d24f8070e356ac90fac5df5692432ee3e0ea0c1558e6bb78e76aade721cf68")
+addappid(2830504,0,"ab46ce36b2648707946bc7ff2e1562491060503e25487e60fcc60e4a692f5c7b")
+addappid(2830505,0,"3a2de50a20a1204b2d32c00deb6b5f5082560e4e7ef80e78236b4a25299a5486")
+addappid(2830506,0,"1de09055224b1a241e117533dccab28c234008abe60653c30cd44f9ebf48e9f5")
+addappid(2830507,0,"fbde4f908358404a6544359f9c8c402bb8a40a336fb00fa92074cf4bf30143b2")
+addappid(2830508,0,"f1808e29d18c4de5461e51d4e7d3f65b9a7eee466b91359150ad4588bdbbabe6")
+addappid(2830509,0,"7d4266463917726e639937e41582b2ba629c710a3a415dfaea90997ffd35e241")
 
--- Main AppID
-addappid(2677660, 1, "4b22dac540d24fab8fe0ce68b7166d192bd13bb6488c9a18de875706c9b32011")
-addtoken(2677660, "11743623575295112529")
-
--- Main Depots
-addappid(2677661, 1, "0abf0860b7a1d53e5ae7b3ed8d7e97e668fa5619be6a6ad74c140c53871868bd") -- (windows, 64-bit)
-setManifestid(2677661, "6237523072047006667", 124760039362)
-addappid(2677662, 1, "b4089fc44110008f2b07bc97f30e298c1ae457fa69964cdb239f480259baec84")
-setManifestid(2677662, "5084568945809971997", 411047360)
-addappid(2830501, 1, "43cf88c0f48b755f8cb8d54bf6f3a04a2ed5551120d29b81c8ae098f26edbb0e")
-setManifestid(2830501, "6601360459876353050", 224920844)
-addappid(2830502, 1, "c9fbb0008c00893bad5a3815793a4439d6a7d3ebcdc9bc6fe187a30f2ab326c7") -- German
-setManifestid(2830502, "8007413738434142917", 307966856)
-addappid(2830503, 1, "02d24f8070e356ac90fac5df5692432ee3e0ea0c1558e6bb78e76aade721cf68") -- Spanish
-setManifestid(2830503, "830671202293683776", 318175389)
-addappid(2830504, 1, "ab46ce36b2648707946bc7ff2e1562491060503e25487e60fcc60e4a692f5c7b") -- Latam
-setManifestid(2830504, "6629109004288775421", 316924506)
-addappid(2830505, 1, "3a2de50a20a1204b2d32c00deb6b5f5082560e4e7ef80e78236b4a25299a5486") -- French
-setManifestid(2830505, "2755672558638797836", 307566930)
-addappid(2830506, 1, "1de09055224b1a241e117533dccab28c234008abe60653c30cd44f9ebf48e9f5") -- Italian
-setManifestid(2830506, "3017367679564545429", 323221633)
-addappid(2830507, 1, "fbde4f908358404a6544359f9c8c402bb8a40a336fb00fa92074cf4bf30143b2") -- Japanese
-setManifestid(2830507, "278507716317809228", 311372114)
-addappid(2830508, 1, "f1808e29d18c4de5461e51d4e7d3f65b9a7eee466b91359150ad4588bdbbabe6") -- Polish
-setManifestid(2830508, "6454515309235511181", 306280503)
-addappid(2830509, 1, "7d4266463917726e639937e41582b2ba629c710a3a415dfaea90997ffd35e241") -- Brazilian
-setManifestid(2830509, "6972604076386304594", 304321401)
-
--- DLC's (no depot keys required)
-addappid(2830510) -- Indiana Jones and the Great Circle - Traveling Suit Outfit
-addappid(2830520) -- Indiana Jones and the Great Circle - Lion Tamer Whip
-addappid(2832880) -- Indiana Jones and the Great Circle - Temple of Doom™ Outfit
-addappid(3138470) -- Indiana Jones and the Great Circle: Digital Premium Upgrade
-
--- DLC's (with depot keys)
--- Indiana Jones and the Great Circle™: The Order of Giants (AppID: 2830500)
-addappid(2830500)
-addtoken(2830500, "27111668089869839")
-addappid(2830500, 1, "c51fae5748ae85fe9a754c5e7cb2bb783d4602005897d3401b00104492820eef") -- (windows, 64-bit)
-setManifestid(2830500, "4724397213563997330", 9454257803)
-
--- Shared Depots (Runtimes / Launchers / ETC)
-addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
-
--- Missing Depots (We don't have the keys yet lol): 2677663
+--[[
+This file belongs to Ahmeds Bot Discord Server
+-----> join discord Server : https://discord.gg/sv6EGxCRnC
+]]
