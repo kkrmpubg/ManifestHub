a224be515d91da66278373fd1a6d9abb1ea8db79
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-08-31 08:58:11 +0800

    Add files via upload

--- 2244210.lua
+++ 2244210.lua
@@ -1,14 +1,14 @@
 -- Generated with Luie @ https://lua.tools/
 -- 2244210 - Echoes of Aincrad
--- Generated 2026-07-10 02:43:04 UTC
+-- Generated 2026-08-31 00:30:29 UTC
 -- # Depots (Total/DLC/Shared): 5/2/2
 
 -- Main AppID
-addappid(2244210)
+addappid(2244210, 1, "75aa96c710bcec66bc333c4febb98ddafb89d44bbc348d45dc2e4ab039c71a3a")
 
 -- Main Depots
 addappid(2244211, 1, "105c2f87ad76d3549179dfbeb54c0248a1bde5a72ba14d74714e480d4706f57e")
-setManifestid(2244211, "1527046485629517929", 38561768950)
+setManifestid(2244211, "6405404564532695691", 39148515477)
 
 -- DLC's (no depot keys required)
 addappid(4249240) -- Echoes of Aincrad - Death Game Mode Early Unlock
@@ -23,9 +23,11 @@ addappid(4337770) -- Echoes of Aincrad - Ultimate Upgrade Pack
 addappid(3443750)
 addappid(3443750, 1, "85140ce089e2a2c387357acfbc4408d975400ab69786255011f7a1d0bc406d2d")
 setManifestid(3443750, "310538696736326014", 4642644659)
+-- Echoes of Aincrad - Anime Unanswered//butterfly (HD Version) (AppID: 3996330)
+addappid(3996330)
+addappid(3996330, 1, "c3328094b1d75686feb7c847e767f15dd73e268e700d36090b8e8b9052aa0900")
+setManifestid(3996330, "6174041972682850761", 14211664023)
 
 -- Shared Depots (Runtimes / Launchers / ETC)
 addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
 addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- (windows)
-
--- Missing Depots (We don't have the keys yet lol): 3996330
