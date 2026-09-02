597fff5a609340969f8d059e0c9bbc11260f04af
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-09-03 06:47:25 +0800

    Add files via upload

---
+++ 3751260.lua
@@ -0,0 +1,19 @@
+-- Generated with Luie @ https://lua.tools/
+-- 3751260 - The Blood of Dawnwalker
+-- Generated 2026-09-02 22:15:43 UTC
+-- # Depots (Total/DLC/Shared): 3/0/1
+
+-- Main AppID
+addappid(3751260)
+
+-- Main Depots
+addappid(3751261, 1, "fe44cc16911c3d5dabb2b862a70f1b8b58ee79e1da1f47b8bf03af487c17e8fc")
+setManifestid(3751261, "116071877880222957", 58350822521)
+
+-- DLC's (no depot keys required)
+addappid(4417550) -- The Blood of Dawnwalker - Eclipse Edition Content
+
+-- Shared Depots (Runtimes / Launchers / ETC)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- (windows)
+
+-- Missing Depots (We don't have the keys yet lol): 3751263
