ae7e94f7b008561e5d09331da0cb90d60b792453
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-09-08 20:59:56 +0800

    Add files via upload

---
+++ 3009300.lua
@@ -0,0 +1,20 @@
+-- 3009300's Lua and Manifest Created by Hubcap Manifest
+-- Teamfight Manager 2
+-- Created: September 02, 2026 at 21:41:58 EDT
+-- Website: https://hubcapmanifest.com/
+-- Total Depots: 4
+-- Total DLCs: 0
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(3009300, 1, "959ccbd7f66f8670fc1d6b9124421955f1f7c31b953050f12bc234ec7ee7128b") -- Teamfight Manager 2
+-- MAIN APP DEPOTS
+addappid(3009301, 1, "b23fe424cc9f269898756472a513ce0395b175590259861bb8a68429fdc53fd2") -- Depot 3009301
+setManifestid(3009301, "2954263401846710061", 1214392334)
+addappid(3009302, 1, "1fe745e6e17d2a1756a1141166288b75230a135dcace909747b21a7b3b3d9e5c") -- Depot 3009302
+setManifestid(3009302, "7325928956037365935", 1199126159)
+addappid(3009303, 1, "7560f11036a03541c398147f2d257f514fc1c3b21e85bda7e30024f0103e1913") -- Depot 3009303
+setManifestid(3009303, "5650811284623115416", 1208068645)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "5753583882400741046", 25674515)
