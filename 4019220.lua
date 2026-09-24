4eec28bc53fe12ebf0d11d1fbab2b32eecadc858
kkrmpubg <110752592+kkrmpubg@users.noreply.github.com>
2026-09-24 13:16:44 +0800

    Add files via upload

---
+++ 4019220.lua
@@ -0,0 +1,13 @@
+-- Generated with Luie @ https://lua.tools/
+-- 4019220 - Dressmaker
+-- Generated 2026-09-24 05:07:46 UTC
+-- # Depots (Total/DLC/Shared): 2/0/0
+
+-- Main AppID
+addappid(4019220)
+
+-- Main Depots
+addappid(4019221, 1, "92df2b5bbee73742310eff7c946dfd46e2d34b32f1328938726bccd2c3e9969f") -- (windows)
+setManifestid(4019221, "4928482405075635734", 1276136871)
+addappid(4019222, 1, "60064d93e8a1df286c473b99f3ebf3ccd8f7660a1e05f2d47efe65015ca2c51d") -- (macos)
+setManifestid(4019222, "7959595438791687317", 1307379016)
